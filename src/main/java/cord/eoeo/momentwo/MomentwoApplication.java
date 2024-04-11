e671364400919ac1306a8a1b83b24dba64321386
CordHouse <qkqktl5310@gmail.com>
2024-04-12 03:16:47 +0900

    init: 프로젝트 생성

---
+++ src/main/java/cord/eoeo/momentwo/MomentwoApplication.java
@@ -0,0 +1,13 @@
+package cord.eoeo.momentwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomentwoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MomentwoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cord/eoeo/momentwo/MomentwoApplicationTests.java
@@ -0,0 +1,13 @@
+package cord.eoeo.momentwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomentwoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
