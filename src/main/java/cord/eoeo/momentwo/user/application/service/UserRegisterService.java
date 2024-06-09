05e660a7e41e617ccc3acace94c12b5d3470348f
CordHouse <qkqktl5310@gmail.com>
2024-06-10 05:10:18 +0900

    #10 feat : 회원가입 비동기 로직 적용

--- src/main/java/cord/eoeo/momentwo/user/application/service/UserRegisterService.java
+++ src/main/java/cord/eoeo/momentwo/user/application/service/UserRegisterService.java
@@ -1,30 +1,37 @@
 package cord.eoeo.momentwo.user.application.service;
 
 import cord.eoeo.momentwo.user.adapter.dto.in.UserRegisterRequestDto;
-import cord.eoeo.momentwo.user.advice.exception.DuplicateNicknameException;
-import cord.eoeo.momentwo.user.advice.exception.DuplicateUsernameException;
 import cord.eoeo.momentwo.user.application.port.in.UserRegisterUseCase;
 import cord.eoeo.momentwo.user.application.port.out.PasswordEncoder;
+import cord.eoeo.momentwo.user.application.port.out.UserRegisterAsync;
 import cord.eoeo.momentwo.user.application.port.out.UserRepository;
 import cord.eoeo.momentwo.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.concurrent.CompletableFuture;
+
 @Service
 @RequiredArgsConstructor
 public class UserRegisterService implements UserRegisterUseCase {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final UserRegisterAsync userRegisterAsync;
+
     @Transactional
     @Override
     public void register(UserRegisterRequestDto userRegisterRequestDto) {
         // 유저 이메일(Id) 중복체크
-        checkUserIdDuplicate(userRegisterRequestDto.getUsername());
+        CompletableFuture<Void> checkUsernameFuture = userRegisterAsync.checkUsernameDuplicate(userRegisterRequestDto.getUsername());
 
         // 유저 닉네임 중복 체크
-        checkUserNicknameDuplicate(userRegisterRequestDto.getNickname());
+        CompletableFuture<Void> checkNicknameFuture = userRegisterAsync.checkUserNicknameDuplicate(userRegisterRequestDto.getNickname());
 
+        // 두 중복 체크가 정상적으로 통과한 경우
+        // join() -> 작업이 정상적으로 동작했을 때 결과를 반환한다.
+        // 예외 시 CompletionException 을 발생시킨다.
+        CompletableFuture.allOf(checkUsernameFuture, checkNicknameFuture).join();
         User newUser = new User(
                 userRegisterRequestDto.getName(),
                 userRegisterRequestDto.getUsername(),
@@ -34,17 +41,5 @@ public class UserRegisterService implements UserRegisterUseCase {
                 userRegisterRequestDto.getPhone()
         );
         userRepository.save(newUser);
-    }
-
-    private void checkUserIdDuplicate(String username) {
-        if(userRepository.existsByUsername(username)) {
-            throw new DuplicateUsernameException();
-        }
-    }
-
-    private void checkUserNicknameDuplicate(String nickname) {
-        if(userRepository.existsByNickname(nickname)) {
-            throw new DuplicateNicknameException();
-        }
     }
 }
