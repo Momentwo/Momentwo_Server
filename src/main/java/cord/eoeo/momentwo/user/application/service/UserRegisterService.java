d31051535bd6eecd46c0e5a89b049fdaeafc447d
CordHouse <qkqktl5310@gmail.com>
2024-06-11 02:32:25 +0900

    #15 fix : 코드 수정
    - sout 구문 삭제
    - 라인에 맞춰 들여쓰기 수정

--- src/main/java/cord/eoeo/momentwo/user/application/service/UserRegisterService.java
+++ src/main/java/cord/eoeo/momentwo/user/application/service/UserRegisterService.java
@@ -23,10 +23,12 @@ public class UserRegisterService implements UserRegisterUseCase {
     @Override
     public void register(UserRegisterRequestDto userRegisterRequestDto) {
         // 유저 이메일(Id) 중복체크
-        CompletableFuture<Void> checkUsernameFuture = userRegisterAsync.checkUsernameDuplicate(userRegisterRequestDto.getUsername());
+        CompletableFuture<Void> checkUsernameFuture = userRegisterAsync
+                .checkUsernameDuplicate(userRegisterRequestDto.getUsername());
 
         // 유저 닉네임 중복 체크
-        CompletableFuture<Void> checkNicknameFuture = userRegisterAsync.checkUserNicknameDuplicate(userRegisterRequestDto.getNickname());
+        CompletableFuture<Void> checkNicknameFuture = userRegisterAsync
+                .checkUserNicknameDuplicate(userRegisterRequestDto.getNickname());
 
         // 두 중복 체크가 정상적으로 통과한 경우
         // join() -> 작업이 정상적으로 동작했을 때 결과를 반환한다.
