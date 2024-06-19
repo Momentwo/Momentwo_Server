e09080dadca81136dece80ac632d12bbc31984dc
CordHouse <qkqktl5310@gmail.com>
2024-06-20 01:50:42 +0900

    #19 feat : 이메일, 별명 검증 DTO

---
+++ src/main/java/cord/eoeo/momentwo/user/adapter/dto/in/EmailAvailabilityDto.java
@@ -0,0 +1,18 @@
+package cord.eoeo.momentwo.user.adapter.dto.in;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmailAvailabilityDto {
+    @NotBlank(message = "사용할 이메일를 입력해주세요.")
+    @Pattern(regexp = "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$",
+            message = "이메일 형식을 지켜주세요.")
+    private String username;
+}
