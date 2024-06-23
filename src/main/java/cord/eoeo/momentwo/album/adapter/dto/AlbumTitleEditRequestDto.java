c633645825c1b5e9d8334dc285c4cc0cad2f84ca
CordHouse <qkqktl5310@gmail.com>
2024-06-23 19:47:35 +0900

    #20 feat : 앨범 DTO 추가
    - 앨범 생성 요청
    - 앨범 수정 요청

---
+++ src/main/java/cord/eoeo/momentwo/album/adapter/dto/AlbumTitleEditRequestDto.java
@@ -0,0 +1,12 @@
+package cord.eoeo.momentwo.album.adapter.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AlbumTitleEditRequestDto {
+    private String editTitle;
+}
