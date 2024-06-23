df68ff0e00535c48e8fb35b7e44fa94c79864ed7
CordHouse <qkqktl5310@gmail.com>
2024-06-23 19:49:42 +0900

    #20 feat : 앨범 유스케이스

---
+++ src/main/java/cord/eoeo/momentwo/album/application/port/in/AlbumUseCase.java
@@ -0,0 +1,12 @@
+package cord.eoeo.momentwo.album.application.port.in;
+
+import cord.eoeo.momentwo.album.adapter.dto.AlbumCreateRequestDto;
+import cord.eoeo.momentwo.album.adapter.dto.AlbumTitleEditRequestDto;
+
+public interface AlbumUseCase {
+    void createAlbums(AlbumCreateRequestDto albumCreateRequestDto);
+
+    void editAlbumsTitle(long id, AlbumTitleEditRequestDto albumTitleEditRequestDto);
+
+    void deleteAlbums(long id);
+}
