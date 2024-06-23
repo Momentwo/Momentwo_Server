9461797ed4f1fc56beaa6e3c8dad6b61070d3246
CordHouse <qkqktl5310@gmail.com>
2024-06-23 19:49:55 +0900

    #20 feat : 앨범 컨트롤러 추가

---
+++ src/main/java/cord/eoeo/momentwo/album/adapter/in/AlbumController.java
@@ -0,0 +1,38 @@
+package cord.eoeo.momentwo.album.adapter.in;
+
+import cord.eoeo.momentwo.album.adapter.dto.AlbumCreateRequestDto;
+import cord.eoeo.momentwo.album.adapter.dto.AlbumTitleEditRequestDto;
+import cord.eoeo.momentwo.album.application.port.in.AlbumUseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+public class AlbumController {
+    private final AlbumUseCase albumUseCase;
+
+    // 앨범 생성
+    @PostMapping("/albums/create")
+    @ResponseStatus(HttpStatus.OK)
+    public void createAlbums(@RequestBody @Valid AlbumCreateRequestDto albumCreateRequestDto) {
+        albumUseCase.createAlbums(albumCreateRequestDto);
+    }
+
+    // 앨범 수정
+    @PutMapping("/albums/edit/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void editAlbumsTitle(@PathVariable long id,
+                                @RequestBody @Valid AlbumTitleEditRequestDto albumTitleEditRequestDto) {
+        albumUseCase.editAlbumsTitle(id, albumTitleEditRequestDto);
+    }
+
+    // 앨범 삭제
+    @DeleteMapping("/albums/delete/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteAlbums(@PathVariable long id) {
+        albumUseCase.deleteAlbums(id);
+    }
+}
