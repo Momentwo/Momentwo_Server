f29c545d9d72c1a799edf62fe674a34a5f300ba1
CordHouse <qkqktl5310@gmail.com>
2024-07-12 23:02:40 +0900

    #22 feat : 앨범 인포 레포추가

---
+++ src/main/java/cord/eoeo/momentwo/member/application/port/out/GetAlbumInfo.java
@@ -0,0 +1,31 @@
+package cord.eoeo.momentwo.member.application.port.out;
+
+import cord.eoeo.momentwo.album.domain.Album;
+import cord.eoeo.momentwo.member.domain.Member;
+import cord.eoeo.momentwo.member.domain.MemberAlbumGrade;
+import cord.eoeo.momentwo.user.domain.User;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+public interface GetAlbumInfo {
+    CompletableFuture<Album> getAlbum(long id);
+
+    // 관리자급 권한 유저인지 확인
+    void checkAuthorityAdmin(long albumId, long userId);
+
+    // 앨범 접근 권한 변경
+    void editGrade(long albumId, long userId, String rules);
+
+    List<String> getAlbumMemberList(long albumId);
+
+    void changeRules(long albumId, User owner, User target, String rules);
+
+    Member getAlbumMemberInfo(long albumId, long userId);
+
+    void doKickMember(long albumId, User owner, User kickedUser);
+
+    void assignAdmin(long albumId, User changeUser, MemberAlbumGrade rules);
+
+    boolean isAlbumOut(long albumId, User selfUser);
+}
