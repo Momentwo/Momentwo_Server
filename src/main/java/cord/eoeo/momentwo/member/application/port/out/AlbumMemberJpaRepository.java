e2cb2c413e5f59fc0680c9a1da02dd57b31d19d5
CordHouse <qkqktl5310@gmail.com>
2024-07-12 23:01:40 +0900

    #22 feat : 멤버 레포지토리 생성

---
+++ src/main/java/cord/eoeo/momentwo/member/application/port/out/AlbumMemberJpaRepository.java
@@ -0,0 +1,26 @@
+package cord.eoeo.momentwo.member.application.port.out;
+
+import cord.eoeo.momentwo.member.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface AlbumMemberJpaRepository extends JpaRepository<Member, Long> {
+    @Query("SELECT u.nickname FROM Member m JOIN m.user u JOIN m.album a WHERE a.id = :albumId")
+    List<String> findNicknameByAlbum(long albumId);
+
+    @Query("SELECT m FROM Member m JOIN m.user u JOIN m.album a WHERE a.id = :albumId and u.id = :userId")
+    Optional<Member> findMemberGradeByAlbumIdAndUserId(long albumId, long userId);
+
+    @Query("SELECT u.nickname FROM Member m JOIN m.user u JOIN m.album a WHERE a.id = :albumId")
+    List<String> getAllNicknameList(long albumId);
+
+    void deleteById(long id);
+
+    @Modifying
+    @Query("DELETE FROM Member m WHERE m.album.id = :albumId AND m.user.id = :userId")
+    int deleteByAlbumIdAndUserId(long albumId, long userId);
+}
