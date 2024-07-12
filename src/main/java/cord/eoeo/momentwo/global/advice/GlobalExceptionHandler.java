cb4ec9d61f0058d745bf0128fcfb8fbeb023d70c
CordHouse <qkqktl5310@gmail.com>
2024-07-12 22:58:47 +0900

    #22 fix : 글로벌 예외처리 수정
    - 비동기 예외처리 구문 삭제
    (비동기 예외처리가 되지 않는 문제의 근원)

--- src/main/java/cord/eoeo/momentwo/global/advice/GlobalExceptionHandler.java
+++ src/main/java/cord/eoeo/momentwo/global/advice/GlobalExceptionHandler.java
@@ -1,7 +1,5 @@
 package cord.eoeo.momentwo.global.advice;
 
-import cord.eoeo.momentwo.user.advice.exception.NotFoundUserException;
-import cord.eoeo.momentwo.user.advice.exception.PasswordMisMatchException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,27 +7,13 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.Objects;
-import java.util.concurrent.CompletionException;
 
 @RestControllerAdvice
-public class globalExceptionHandler {
+public class GlobalExceptionHandler {
     // DTO에서 패턴에 따른 예외처리 적용
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public String dtoValidation(final MethodArgumentNotValidException e) {
         return Objects.requireNonNull(e.getFieldError()).getDefaultMessage();
-    }
-
-    // Async 핸들러
-    @ExceptionHandler(CompletionException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public void completionHandlerException(CompletionException ex) {
-        Throwable cause = ex.getCause();
-        if(cause instanceof NotFoundUserException) {
-            throw new NotFoundUserException();
-        }
-        else {
-            throw new PasswordMisMatchException();
-        }
     }
 }
