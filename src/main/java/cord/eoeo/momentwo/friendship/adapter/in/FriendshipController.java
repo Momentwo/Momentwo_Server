8ecd3e2dcf63769708fef92c1911c8e19c910467
CordHouse <qkqktl5310@gmail.com>
2024-07-20 21:45:33 +0900

    #37 feat : 친구목록 컨트롤러 추가
    - 요청
    - 응답

---
+++ src/main/java/cord/eoeo/momentwo/friendship/adapter/in/FriendshipController.java
@@ -0,0 +1,30 @@
+package cord.eoeo.momentwo.friendship.adapter.in;
+
+import cord.eoeo.momentwo.friendship.adapter.in.dto.RequestFriendshipDto;
+import cord.eoeo.momentwo.friendship.adapter.in.dto.ResponseFriendshipDto;
+import cord.eoeo.momentwo.friendship.application.port.in.FriendshipUseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+public class FriendshipController {
+    private final FriendshipUseCase friendshipUseCase;
+
+    // 요청
+    @PostMapping("/friendship/request")
+    @ResponseStatus(HttpStatus.OK)
+    public void requestFriendship(@ModelAttribute @Valid RequestFriendshipDto requestFriendshipDto) {
+        friendshipUseCase.requestFriendship(requestFriendshipDto);
+    }
+
+    // 친구요청에 대한 응답 (수락 & 거절)
+    @PostMapping("/friendship/response")
+    @ResponseStatus(HttpStatus.OK)
+    public void responseFriendship(@RequestBody @Valid ResponseFriendshipDto responseFriendshipDto) {
+        friendshipUseCase.responseFriendship(responseFriendshipDto);
+    }
+}
