00fe284e131f18db9de4bd71065f3fb8715d1639
CordHouse <qkqktl5310@gmail.com>
2024-07-20 21:45:14 +0900

    #37 feat : 친구목록 비즈니스 로직
    - 요청
    - 응답

---
+++ src/main/java/cord/eoeo/momentwo/friendship/application/service/FriendshipService.java
@@ -0,0 +1,60 @@
+package cord.eoeo.momentwo.friendship.application.service;
+
+import cord.eoeo.momentwo.friendship.adapter.in.dto.RequestFriendshipDto;
+import cord.eoeo.momentwo.friendship.adapter.in.dto.ResponseFriendshipDto;
+import cord.eoeo.momentwo.friendship.application.port.in.FriendshipUseCase;
+import cord.eoeo.momentwo.friendship.application.port.out.FriendshipProcess;
+import cord.eoeo.momentwo.user.advice.exception.NotFoundUserException;
+import cord.eoeo.momentwo.user.application.port.out.GetAuthentication;
+import cord.eoeo.momentwo.user.application.port.out.UserRepository;
+import cord.eoeo.momentwo.user.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class FriendshipService implements FriendshipUseCase {
+    private final UserRepository userRepository;
+    private final GetAuthentication getAuthentication;
+    private final FriendshipProcess friendshipProcess;
+
+    // TODO 이미 친구 요청이 된 상태라면 친구 요청이 진행되면 안된다. -> 예외처리 필요
+    // 친구요청
+    @Override
+    @Transactional
+    public void requestFriendship(RequestFriendshipDto requestFriendshipDto) {
+        User fromUser = userRepository.findByNickname(getAuthentication.getAuthentication().getName()).orElseThrow(
+                NotFoundUserException::new
+        );
+        // 사용자 조회
+        User toUser = userRepository.findByNickname(requestFriendshipDto.getNickname()).orElseThrow(
+                NotFoundUserException::new
+        );
+
+        // from -> to (true)
+        // to -> from (false)
+        friendshipProcess.friendsRequest(fromUser, toUser, true);
+        friendshipProcess.friendsRequest(toUser, fromUser, false);
+    }
+
+    // 친구요청 응답 (수락 & 거절)
+    @Override
+    @Transactional
+    public void responseFriendship(ResponseFriendshipDto responseFriendshipDto) {
+        User responseUser = userRepository.findByNickname(getAuthentication.getAuthentication().getName()).orElseThrow(
+                NotFoundUserException::new
+        );
+        User requestUser = userRepository.findByNickname(responseFriendshipDto.getNickname()).orElseThrow(
+                NotFoundUserException::new
+        );
+
+        // 수락 시 false -> true
+        // 거절 시 data 삭제
+        friendshipProcess.responseProcess(
+                responseUser,
+                requestUser,
+                responseFriendshipDto.getAccept()
+        );
+    }
+}
