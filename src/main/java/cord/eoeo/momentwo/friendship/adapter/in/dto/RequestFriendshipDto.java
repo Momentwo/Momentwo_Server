0f5d623051a804c56237a32e64aa4fd5310a0309
CordHouse <qkqktl5310@gmail.com>
2024-07-20 21:43:33 +0900

    #37 feat : 친구 요청 DTO 추가

---
+++ src/main/java/cord/eoeo/momentwo/friendship/adapter/in/dto/RequestFriendshipDto.java
@@ -0,0 +1,12 @@
+package cord.eoeo.momentwo.friendship.adapter.in.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RequestFriendshipDto {
+    private String nickname;
+}
