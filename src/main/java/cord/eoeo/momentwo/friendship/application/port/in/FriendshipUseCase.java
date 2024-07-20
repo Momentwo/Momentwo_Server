4397e584cea73fb78f8fc306e79d49406e60717c
CordHouse <qkqktl5310@gmail.com>
2024-07-20 21:45:23 +0900

    #37 feat : 친구목록 유스케이스
    - 요청
    - 응답

---
+++ src/main/java/cord/eoeo/momentwo/friendship/application/port/in/FriendshipUseCase.java
@@ -0,0 +1,10 @@
+package cord.eoeo.momentwo.friendship.application.port.in;
+
+import cord.eoeo.momentwo.friendship.adapter.in.dto.RequestFriendshipDto;
+import cord.eoeo.momentwo.friendship.adapter.in.dto.ResponseFriendshipDto;
+
+public interface FriendshipUseCase {
+    void requestFriendship(RequestFriendshipDto requestFriendshipDto);
+
+    void responseFriendship(ResponseFriendshipDto responseFriendshipDto);
+}
