f3326a7e7c36b0c8fa11510451c89ab83735c554
CordHouse <qkqktl5310@gmail.com>
2024-07-22 00:25:57 +0900

    #39 feat : 스스로에게 요청 예외

--- src/main/java/cord/eoeo/momentwo/friendship/advice/FriendshipExceptionHandler.java
+++ src/main/java/cord/eoeo/momentwo/friendship/advice/FriendshipExceptionHandler.java
@@ -3,6 +3,7 @@ package cord.eoeo.momentwo.friendship.advice;
 import cord.eoeo.momentwo.friendship.advice.exception.AlreadyFriendshipException;
 import cord.eoeo.momentwo.friendship.advice.exception.AlreadyFriendshipRequestException;
 import cord.eoeo.momentwo.friendship.advice.exception.NotFoundFriendshipRequestException;
+import cord.eoeo.momentwo.friendship.advice.exception.SelfRequestException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -26,5 +27,11 @@ public class FriendshipExceptionHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public String alreadyFriendshipRequestException() {
         return "이미 친구요청이 되어 있습니다.";
+    }
+
+    @ExceptionHandler(SelfRequestException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String selfInviteException() {
+        return "스스로에게 요청할 수 없습니다.";
     }
 }
