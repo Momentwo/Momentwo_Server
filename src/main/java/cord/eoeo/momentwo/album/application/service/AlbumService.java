4e7fdfc180159d4640988b5fffa8fade3f1e6750
CordHouse <qkqktl5310@gmail.com>
2024-07-23 16:37:57 +0900

    #41 feat : 앨범 비즈니스 로직 수정
    - 앨범 생성하면서 멤버를 초대하고
    - 앨범을 생성하면서 관리자 권한을 부여한다.

--- src/main/java/cord/eoeo/momentwo/album/application/service/AlbumService.java
+++ src/main/java/cord/eoeo/momentwo/album/application/service/AlbumService.java
@@ -6,6 +6,12 @@ import cord.eoeo.momentwo.album.advice.exception.NotFoundAlbumException;
 import cord.eoeo.momentwo.album.application.port.in.AlbumUseCase;
 import cord.eoeo.momentwo.album.application.port.out.AlbumRepository;
 import cord.eoeo.momentwo.album.domain.Album;
+import cord.eoeo.momentwo.member.application.port.out.AlbumMemberInvite;
+import cord.eoeo.momentwo.member.domain.MemberAlbumGrade;
+import cord.eoeo.momentwo.user.advice.exception.NotFoundUserException;
+import cord.eoeo.momentwo.user.application.port.out.GetAuthentication;
+import cord.eoeo.momentwo.user.application.port.out.UserRepository;
+import cord.eoeo.momentwo.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,12 +20,27 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class AlbumService implements AlbumUseCase {
     private final AlbumRepository albumRepository;
+    private final AlbumMemberInvite albumMemberInvite;
+    private final UserRepository userRepository;
+    private final GetAuthentication getAuthentication;
 
     @Transactional
     @Override
     public void createAlbums(AlbumCreateRequestDto albumCreateRequestDto) {
+        User admin = userRepository.findByNickname(getAuthentication.getAuthentication().getName())
+                .orElseThrow(NotFoundUserException::new);
+        // 앨범 생성
         Album newAlbum = new Album(albumCreateRequestDto.getCreateTitle());
         albumRepository.save(newAlbum);
+
+        // 앨범 생성자 관리자 권한 부여
+        albumMemberInvite.invite(newAlbum, admin, MemberAlbumGrade.ROLE_ALBUM_ADMIN);
+
+        // 앨범에 초대된 멤버 권한 부여
+        albumCreateRequestDto.getDoInviteNicknameList().forEach(nickname -> {
+            User inviteUser = userRepository.findByNickname(nickname).orElseThrow();
+            albumMemberInvite.invite(newAlbum, inviteUser);
+        });
     }
 
     @Transactional
