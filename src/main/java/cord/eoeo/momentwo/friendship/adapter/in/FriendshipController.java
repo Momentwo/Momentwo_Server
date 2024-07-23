cdfa7cc855b3022a6e485635b4aacf6039b31239
CordHouse <qkqktl5310@gmail.com>
2024-07-23 15:34:57 +0900

    #41 feat : 친구 조회 컨트롤러 추가
    - 친구 목록조회
    - 보낸 친구목록 조회
    - 받은 친구목록 조회

--- src/main/java/cord/eoeo/momentwo/friendship/adapter/in/FriendshipController.java
+++ src/main/java/cord/eoeo/momentwo/friendship/adapter/in/FriendshipController.java
@@ -1,7 +1,6 @@
 package cord.eoeo.momentwo.friendship.adapter.in;
 
-import cord.eoeo.momentwo.friendship.adapter.in.dto.RequestFriendshipDto;
-import cord.eoeo.momentwo.friendship.adapter.in.dto.ResponseFriendshipDto;
+import cord.eoeo.momentwo.friendship.adapter.in.dto.*;
 import cord.eoeo.momentwo.friendship.application.port.in.FriendshipUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -28,9 +27,31 @@ public class FriendshipController {
         friendshipUseCase.responseFriendship(responseFriendshipDto);
     }
 
+    // 친구 요청 취소
     @DeleteMapping("/friendship/request/cancel")
     @ResponseStatus(HttpStatus.OK)
     public void requestFriendshipCancel(@ModelAttribute @Valid RequestFriendshipDto requestFriendshipDto) {
         friendshipUseCase.requestFriendshipCancel(requestFriendshipDto);
+    }
+
+    // 친구목록 조회
+    @GetMapping("/friendship")
+    @ResponseStatus(HttpStatus.OK)
+    public FriendshipAllListResponseDto getFriendship() {
+        return friendshipUseCase.getFriendship();
+    }
+
+    // 보낸 친구 요청 조회
+    @GetMapping("/friendship/send")
+    @ResponseStatus(HttpStatus.OK)
+    public FriendshipSendListResponseDto getFriendshipSend() {
+        return friendshipUseCase.getFriendshipSend();
+    }
+
+    // 받은 친구 요청 조회
+    @GetMapping("/friendship/receive")
+    @ResponseStatus(HttpStatus.OK)
+    public FriendshipReceiveListResponseDto getFriendshipReceive() {
+        return friendshipUseCase.getFriendshipReceive();
     }
 }
