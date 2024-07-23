605d2c4f8d93507b3b04ba143136a7c796a9a583
CordHouse <qkqktl5310@gmail.com>
2024-07-23 15:33:04 +0900

    #41 feat : 친구목록 처리 인터페이스 추가
    - 친구 조회
    - 요청한 친구 조회
    - 요청 받은 친구 조회

--- src/main/java/cord/eoeo/momentwo/friendship/adapter/out/FriendshipProcessImpl.java
+++ src/main/java/cord/eoeo/momentwo/friendship/adapter/out/FriendshipProcessImpl.java
@@ -1,5 +1,6 @@
 package cord.eoeo.momentwo.friendship.adapter.out;
 
+import cord.eoeo.momentwo.friendship.adapter.in.dto.*;
 import cord.eoeo.momentwo.friendship.advice.exception.AlreadyFriendshipException;
 import cord.eoeo.momentwo.friendship.advice.exception.NotFoundFriendshipRequestException;
 import cord.eoeo.momentwo.friendship.application.port.out.FriendshipProcess;
@@ -9,6 +10,8 @@ import cord.eoeo.momentwo.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -65,6 +68,30 @@ public class FriendshipProcessImpl implements FriendshipProcess {
     public void requestCancel(User fromUser, User toUser) {
         friendshipRepository.findBySelfJoin(fromUser, toUser).orElseThrow(NotFoundFriendshipRequestException::new);
         friendshipRepository.deleteByFromUserAndToUser(fromUser, toUser);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public FriendshipAllListResponseDto getFriendship(User owner) {
+        List<Friendship> friendships = friendshipRepository.findAllFriendsByUser(owner);
+
+        return new FriendshipAllListResponseDto().toDo(friendships);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public FriendshipSendListResponseDto getFriendshipSend(User owner) {
+        List<Friendship> friendships = friendshipRepository.findSendFriendsByUser(owner);
+
+        return new FriendshipSendListResponseDto().toDo(friendships);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public FriendshipReceiveListResponseDto getFriendshipReceive(User owner) {
+        List<Friendship> friendships = friendshipRepository.findReceiveFriendsByUser(owner);
+
+        return new FriendshipReceiveListResponseDto().toDO(friendships);
     }
 
     @Transactional
