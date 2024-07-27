b9044badffd8e41361b15a80a2b00fac2d906d50
CordHouse <qkqktl5310@gmail.com>
2024-07-28 00:26:38 +0900

    #49 feat : 회원탈퇴 기능 추가

--- src/main/java/cord/eoeo/momentwo/user/application/port/in/UserStatusUseCase.java
+++ src/main/java/cord/eoeo/momentwo/user/application/port/in/UserStatusUseCase.java
@@ -1,6 +1,7 @@
 package cord.eoeo.momentwo.user.application.port.in;
 
 import cord.eoeo.momentwo.config.security.jwt.adapter.out.TokenResponseDto;
+import cord.eoeo.momentwo.user.adapter.dto.in.SignOutRequestDto;
 import cord.eoeo.momentwo.user.adapter.dto.in.UserLoginRequestDto;
 
 import java.util.concurrent.CompletableFuture;
@@ -8,4 +9,5 @@ import java.util.concurrent.CompletableFuture;
 public interface UserStatusUseCase {
     CompletableFuture<TokenResponseDto> signIn(UserLoginRequestDto userLoginRequestDto);
     void blackListToken(String token);
+    void signOut(SignOutRequestDto signOutRequestDto);
 }
