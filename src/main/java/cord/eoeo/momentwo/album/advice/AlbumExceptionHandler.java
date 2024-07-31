6dfa411470e763aae51ab2d81edc27cdea955c95
CordHouse <qkqktl5310@gmail.com>
2024-08-01 00:36:30 +0900

    #53 feat : 앨범 삭제 시 멤버가 존재하는 경우 예외 추가

--- src/main/java/cord/eoeo/momentwo/album/advice/AlbumExceptionHandler.java
+++ src/main/java/cord/eoeo/momentwo/album/advice/AlbumExceptionHandler.java
@@ -1,6 +1,7 @@
 package cord.eoeo.momentwo.album.advice;
 
 import cord.eoeo.momentwo.album.advice.exception.NotAlbumAdminException;
+import cord.eoeo.momentwo.album.advice.exception.NotDeleteAlbumException;
 import cord.eoeo.momentwo.album.advice.exception.NotFoundAlbumException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -19,5 +20,11 @@ public class AlbumExceptionHandler {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public String notAlbumAdminException() {
         return "앨범 관리자만 해당 기능을 사용할 수 있습니다.";
+    }
+
+    @ExceptionHandler(NotDeleteAlbumException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String notDeleteAlbumException() {
+        return "멤버가 존재하는 앨범은 삭제할 수 없습니다.";
     }
 }
