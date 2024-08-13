a558d3d5b2985126d547fb521d6063b9521ec2cc
CordHouse <qkqktl5310@gmail.com>
2024-08-14 02:02:51 +0900

    #57 feat : 앨범 프로필 유스케이스
    - 프로필 이미지 업로드
    - 프로필 이미지 삭제
    - 서브타이틀
    - 서브타이틀 삭제

---
+++ src/main/java/cord/eoeo/momentwo/album/application/port/in/AlbumProfileUseCase.java
@@ -0,0 +1,18 @@
+package cord.eoeo.momentwo.album.application.port.in;
+
+import cord.eoeo.momentwo.album.adapter.dto.AlbumProfileUploadRequestDto;
+import cord.eoeo.momentwo.album.adapter.dto.AlbumSubTitleEditRequestDto;
+
+public interface AlbumProfileUseCase {
+    // 프로필 변경 (업로드)
+    void profileUpload(AlbumProfileUploadRequestDto uploadRequestDto);
+
+    // 프로필 삭제
+    void profileDelete(long albumId);
+
+    // 서브 타이틀 수정
+    void albumSubTitleEdit(AlbumSubTitleEditRequestDto subTitleEditRequestDto);
+
+    // 서브 타이틀 삭제
+    void albumSubTitleDelete(long albumId);
+}
