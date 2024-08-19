c46f8c1b9d79d7fc997696f710de38e3c547f4ed
CordHouse <qkqktl5310@gmail.com>
2024-08-20 05:11:08 +0900

    #60 feat : 이미지 매핑 주소 추가
    - 현재 테스트 용으로 로컬 매핑주소로 추가

---
+++ src/main/java/cord/eoeo/momentwo/config/web/WebConfig.java
@@ -0,0 +1,16 @@
+package cord.eoeo.momentwo.config.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // /images/ 경로로 들어오는 요청을 로컬 파일 경로로 매핑
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("file:///C:/Users/qkqkt/Desktop/momentwo/profile/");
+    }
+}
