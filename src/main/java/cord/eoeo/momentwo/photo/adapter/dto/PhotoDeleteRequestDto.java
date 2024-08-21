bd5507995f1a1de92a3213b98b7706e03422b0ce
CordHouse <qkqktl5310@gmail.com>
2024-08-22 05:20:26 +0900

    #64 feat : 이미지 삭제 요청 DTO

---
+++ src/main/java/cord/eoeo/momentwo/photo/adapter/dto/PhotoDeleteRequestDto.java
@@ -0,0 +1,16 @@
+package cord.eoeo.momentwo.photo.adapter.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PhotoDeleteRequestDto {
+    private long albumId;
+    private List<Long> imagesId;
+    private List<String> imagesUrl;
+}
