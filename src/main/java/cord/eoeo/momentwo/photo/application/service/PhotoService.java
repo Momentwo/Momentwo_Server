7f7e47c015113bc064645410be94e1aad641483e
CordHouse <qkqktl5310@gmail.com>
2024-08-22 05:23:09 +0900

    #64 feat : 사진 비즈니스 로직
    - 업로드 (단일)
    - 삭제 (리스트)
    - 조회 (리스트)

--- src/main/java/cord/eoeo/momentwo/photo/application/service/PhotoService.java
+++ src/main/java/cord/eoeo/momentwo/photo/application/service/PhotoService.java
@@ -1,10 +1,96 @@
 package cord.eoeo.momentwo.photo.application.service;
 
+import cord.eoeo.momentwo.album.application.port.out.AlbumManager;
+import cord.eoeo.momentwo.album.domain.Album;
+import cord.eoeo.momentwo.image.application.port.out.ImageManager;
+import cord.eoeo.momentwo.image.path.ImagePath;
+import cord.eoeo.momentwo.photo.adapter.dto.PhotoDeleteRequestDto;
+import cord.eoeo.momentwo.photo.adapter.dto.PhotoUploadRequestDto;
+import cord.eoeo.momentwo.photo.adapter.dto.PhotoViewListResponseDto;
+import cord.eoeo.momentwo.photo.advice.exception.NotDeleteImageException;
+import cord.eoeo.momentwo.photo.advice.exception.PhotoUploadFailException;
 import cord.eoeo.momentwo.photo.application.port.in.PhotoUseCase;
+import cord.eoeo.momentwo.photo.application.port.out.PhotoRepository;
+import cord.eoeo.momentwo.photo.domain.Photo;
+import cord.eoeo.momentwo.photo.domain.PhotoFormat;
+import cord.eoeo.momentwo.user.advice.exception.NotFoundUserException;
+import cord.eoeo.momentwo.user.application.port.out.GetAuthentication;
+import cord.eoeo.momentwo.user.application.port.out.UserRepository;
+import cord.eoeo.momentwo.user.domain.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class PhotoService implements PhotoUseCase {
+    private final ImageManager imageManager;
+    private final PhotoRepository photoRepository;
+    private final UserRepository userRepository;
+    private final GetAuthentication getAuthentication;
+    private final AlbumManager albumManager;
+
+    @Override
+    @Transactional
+    public void photoUpload(PhotoUploadRequestDto photoUploadRequestDto) {
+        try {
+            // 유저 정보 및 앨범 정보 가져오기
+            User user = userRepository.findByNickname(getAuthentication.getAuthentication().getName())
+                    .orElseThrow(NotFoundUserException::new);
+            Album album = albumManager.getAlbumInfo(photoUploadRequestDto.getAlbumId());
+
+            // 이미지 이름 변환 UUID
+            String newFilename = imageManager
+                    .imageUpload(photoUploadRequestDto.getImages(), ImagePath.SERVER_IMAGE_PATH.getPath())
+                    .get();
+
+            // 타입 정보 찾기
+            String type = PhotoFormat.findPhotoType(newFilename.split("\\.")[1].toUpperCase()).getType();
+
+            Photo newPhoto = new Photo(newFilename, type, user, album);
+
+            photoRepository.save(newPhoto);
+        } catch (Exception e) {
+            throw new PhotoUploadFailException();
+        }
+    }
+
+    @Override
+    @Transactional
+    public void photoDelete(PhotoDeleteRequestDto photoDeleteRequestDto) {
+        if(photoDeleteRequestDto.getImagesId().isEmpty() || photoDeleteRequestDto.getImagesUrl().isEmpty()) {
+            throw new NotDeleteImageException();
+        }
+        // 서버에서 삭제
+        photoRepository.deleteAllByAlbumIdAndPhotoId(
+                photoDeleteRequestDto.getAlbumId(),
+                photoDeleteRequestDto.getImagesId()
+        );
+
+        // 이미지 저장소 삭제
+        photoDeleteRequestDto.getImagesUrl().forEach(image -> {
+            imageManager.imageDelete(ImagePath.SERVER_IMAGE_PATH.getPath(), image);
+        });
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public PhotoViewListResponseDto photoView(long albumId) {
+        List<Photo> photoList = photoRepository.findImageByAlbumId(albumId);
+        List<Photo> imageSaveList = new ArrayList<>();
+
+        photoList.forEach(photo -> {
+            Resource resource = imageManager.imageFileSearch(photo.getImageName()).join();
+            // 이미지 저장소에 있는 이미지만 조회
+            if(resource.exists()) {
+                imageSaveList.add(photo);
+            }
+        });
+
+        return new PhotoViewListResponseDto().toDo(imageSaveList);
+    }
 }
