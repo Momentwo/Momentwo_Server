e747eef01823aea7f472169ce3c65c48d17b4baa
CordHouse <qkqktl5310@gmail.com>
2024-08-22 05:23:17 +0900

    #64 feat : 사진 유스케이스
    - 업로드 (단일)
    - 삭제 (리스트)
    - 조회 (리스트)

--- src/main/java/cord/eoeo/momentwo/photo/application/port/in/PhotoUseCase.java
+++ src/main/java/cord/eoeo/momentwo/photo/application/port/in/PhotoUseCase.java
@@ -1,4 +1,11 @@
 package cord.eoeo.momentwo.photo.application.port.in;
 
+import cord.eoeo.momentwo.photo.adapter.dto.PhotoDeleteRequestDto;
+import cord.eoeo.momentwo.photo.adapter.dto.PhotoUploadRequestDto;
+import cord.eoeo.momentwo.photo.adapter.dto.PhotoViewListResponseDto;
+
 public interface PhotoUseCase {
+    void photoUpload(PhotoUploadRequestDto photoUploadRequestDto);
+    void photoDelete(PhotoDeleteRequestDto photoDeleteRequestDto);
+    PhotoViewListResponseDto photoView(long albumId);
 }
