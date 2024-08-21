ac131d0507a11c407f21e8c154bbb08a163e7634
CordHouse <qkqktl5310@gmail.com>
2024-08-22 05:20:09 +0900

    #64 feat : 이미지를 삭제할 수 없을때 예외처리
    - 삭제 요청 목록이 비어있을 때 예외

--- src/main/java/cord/eoeo/momentwo/photo/advice/PhotoExceptionHandler.java
+++ src/main/java/cord/eoeo/momentwo/photo/advice/PhotoExceptionHandler.java
@@ -1,5 +1,6 @@
 package cord.eoeo.momentwo.photo.advice;
 
+import cord.eoeo.momentwo.photo.advice.exception.NotDeleteImageException;
 import cord.eoeo.momentwo.photo.advice.exception.PhotoUploadFailException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,5 +13,11 @@ public class PhotoExceptionHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public String photoUploadFailException() {
         return "이미지 업로드 실패";
+    }
+
+    @ExceptionHandler(NotDeleteImageException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String notDeleteImageException() {
+        return "삭제할 이미지 목록이 없습니다.";
     }
 }
