be5dbea44276b1f96c53afc2c994a614913f8613
CordHouse <qkqktl5310@gmail.com>
2024-08-22 05:23:23 +0900

    #64 feat : 사진 컨트롤러
    - 업로드 (단일)
    - 삭제 (리스트)
    - 조회 (리스트)

--- src/main/java/cord/eoeo/momentwo/photo/adapter/in/PhotoController.java
+++ src/main/java/cord/eoeo/momentwo/photo/adapter/in/PhotoController.java
@@ -1,11 +1,39 @@
 package cord.eoeo.momentwo.photo.adapter.in;
 
+import cord.eoeo.momentwo.photo.adapter.dto.PhotoDeleteRequestDto;
+import cord.eoeo.momentwo.photo.adapter.dto.PhotoUploadRequestDto;
+import cord.eoeo.momentwo.photo.adapter.dto.PhotoViewListResponseDto;
 import cord.eoeo.momentwo.photo.application.port.in.PhotoUseCase;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/photo")
 public class PhotoController {
     private final PhotoUseCase photoUseCase;
+
+    // 업로드
+    @PostMapping("/upload")
+    @ResponseStatus(HttpStatus.OK)
+    public void photoUpload(@ModelAttribute @Valid PhotoUploadRequestDto photoUploadRequestDto) {
+        photoUseCase.photoUpload(photoUploadRequestDto);
+    }
+
+    // 삭제
+    @DeleteMapping("/delete")
+    @ResponseStatus(HttpStatus.OK)
+    public void photoDelete(@RequestBody @Valid PhotoDeleteRequestDto photoDeleteRequestDto) {
+        photoUseCase.photoDelete(photoDeleteRequestDto);
+    }
+
+    // 조회 (이미지 보기)
+    @GetMapping("/{albumId}/view")
+    @ResponseStatus(HttpStatus.OK)
+    public PhotoViewListResponseDto photoView(@PathVariable long albumId) {
+        return photoUseCase.photoView(albumId);
+    }
 }
