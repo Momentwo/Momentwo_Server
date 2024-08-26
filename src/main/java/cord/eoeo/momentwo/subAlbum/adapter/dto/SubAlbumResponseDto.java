90427442f471a5c04ce18e45216cd74b6b99b116
CordHouse <qkqktl5310@gmail.com>
2024-08-27 04:55:30 +0900

    #72 feat : 서브앨범 조회 응답 DTO 생성
    - 리턴
    - 서브앨범 정보
    - 서브앨범 사진 정보

---
+++ src/main/java/cord/eoeo/momentwo/subAlbum/adapter/dto/SubAlbumResponseDto.java
@@ -0,0 +1,28 @@
+package cord.eoeo.momentwo.subAlbum.adapter.dto;
+
+import cord.eoeo.momentwo.image.adapter.dto.ImageViewResponseDto;
+import cord.eoeo.momentwo.subAlbum.domain.SubAlbum;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SubAlbumResponseDto {
+    private long id;
+    private String subAlbumTitle;
+    private List<ImageViewResponseDto> subTitleImageList;
+
+    public SubAlbumResponseDto toDo(SubAlbum subAlbum) {
+        return new SubAlbumResponseDto(
+                subAlbum.getId(),
+                subAlbum.getSubAlbumTitle(),
+                subAlbum.getPhotos().stream().map(photo -> new ImageViewResponseDto().toDo(photo))
+                        .collect(Collectors.toList())
+        );
+    }
+}
