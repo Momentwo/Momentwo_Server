0a79e54a9df9ad6a901a5bf6194e4b80cbc8af73
CordHouse <qkqktl5310@gmail.com>
2024-08-27 04:53:03 +0900

    #72 feat : 삭제할 서브앨범이 없는 경우 예외처리

--- src/main/java/cord/eoeo/momentwo/subAlbum/advice/SubAlbumExceptionHandler.java
+++ src/main/java/cord/eoeo/momentwo/subAlbum/advice/SubAlbumExceptionHandler.java
@@ -1,5 +1,6 @@
 package cord.eoeo.momentwo.subAlbum.advice;
 
+import cord.eoeo.momentwo.subAlbum.advice.exception.NotDeleteSubAlbumException;
 import cord.eoeo.momentwo.subAlbum.advice.exception.NotFoundSubAlbumException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,5 +13,11 @@ public class SubAlbumExceptionHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public String notFoundSubAlbumException() {
         return "서브 앨범이 존재하지 않습니다.";
+    }
+
+    @ExceptionHandler(NotDeleteSubAlbumException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String notDeleteSubAlbumException() {
+        return "삭제할 서브앨범이 없습니다.";
     }
 }
