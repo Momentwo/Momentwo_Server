40c2cd359bd29540aebb3e921a50b9474fe244f8
CordHouse <qkqktl5310@gmail.com>
2024-08-28 08:22:31 +0900

    #73 fix : 이미지 반환 수정
    - 기존에 Photo 패키지 반환 값 삭제
    - Image 패키지 응답 DTO 수정

--- src/main/java/cord/eoeo/momentwo/image/adapter/dto/ImageViewResponseDto.java
+++ src/main/java/cord/eoeo/momentwo/image/adapter/dto/ImageViewResponseDto.java
@@ -4,7 +4,6 @@ import cord.eoeo.momentwo.photo.domain.Photo;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 
 @Data
 @AllArgsConstructor
@@ -13,15 +12,7 @@ public class ImageViewResponseDto {
     private long id;
     private String imageUrl;
 
-    @Value("${file.dir}")
-    private String dir;
-
-    public ImageViewResponseDto(long id, String url) {
-        this.id = id;
-        this.imageUrl = url;
-    }
-
     public ImageViewResponseDto toDo(Photo photo) {
-        return new ImageViewResponseDto(photo.getId(), dir + photo.getImageName());
+        return new ImageViewResponseDto(photo.getId(), photo.getImageName());
     }
 }
