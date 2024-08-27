6e80989796b3478cb22093f6f90046444758f8af
CordHouse <qkqktl5310@gmail.com>
2024-08-28 08:29:40 +0900

    #73 feat : 사진 조회 반환 수정

--- src/main/java/cord/eoeo/momentwo/photo/application/port/in/PhotoUseCase.java
+++ src/main/java/cord/eoeo/momentwo/photo/application/port/in/PhotoUseCase.java
@@ -1,11 +1,12 @@
 package cord.eoeo.momentwo.photo.application.port.in;
 
+import cord.eoeo.momentwo.image.adapter.dto.ImageViewListResponseDto;
 import cord.eoeo.momentwo.photo.adapter.dto.PhotoDeleteRequestDto;
 import cord.eoeo.momentwo.photo.adapter.dto.PhotoUploadRequestDto;
-import cord.eoeo.momentwo.photo.adapter.dto.PhotoViewListResponseDto;
+import cord.eoeo.momentwo.photo.adapter.dto.PhotoViewRequestDto;
 
 public interface PhotoUseCase {
     void photoUpload(PhotoUploadRequestDto photoUploadRequestDto);
     void photoDelete(PhotoDeleteRequestDto photoDeleteRequestDto);
-    PhotoViewListResponseDto photoView(long albumId);
+    ImageViewListResponseDto photoView(PhotoViewRequestDto photoViewRequestDto);
 }
