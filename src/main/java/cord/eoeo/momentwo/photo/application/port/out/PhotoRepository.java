c5ec3adaac014ca7c1da2b82de4c2ffd7bfa7603
CordHouse <qkqktl5310@gmail.com>
2024-09-04 02:12:58 +0900

    #81 fix : findImageBySubAlbumId 삭제

--- src/main/java/cord/eoeo/momentwo/photo/application/port/out/PhotoRepository.java
+++ src/main/java/cord/eoeo/momentwo/photo/application/port/out/PhotoRepository.java
@@ -1,13 +1,10 @@
 package cord.eoeo.momentwo.photo.application.port.out;
 
 import cord.eoeo.momentwo.photo.domain.Photo;
-
 import java.util.List;
 
 public interface PhotoRepository {
     void save(Photo photo);
-
-    List<Photo> findImageBySubAlbumId(long subAlbumId);
 
     void deleteAllBySubAlbumIdAndPhotoId(long subAlbumId, List<Long> imageIds);
 }
