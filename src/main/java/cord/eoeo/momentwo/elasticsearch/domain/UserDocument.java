b7b2021e80b9e1c395a0765955d05ceed8cd08d7
CordHouse <qkqktl5310@gmail.com>
2024-09-14 07:50:54 +0900

    #93 feat : user document 생성

---
+++ src/main/java/cord/eoeo/momentwo/elasticsearch/domain/UserDocument.java
@@ -0,0 +1,28 @@
+package cord.eoeo.momentwo.elasticsearch.domain;
+
+import cord.eoeo.momentwo.user.domain.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+import javax.persistence.Id;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(indexName = "users")
+public class UserDocument {
+    @Id
+    private long id;
+
+    @Field(type = FieldType.Keyword)
+    private String nickname;
+
+    public UserDocument(User user) {
+        this.id = user.getId();
+        this.nickname = user.getNickname();
+    }
+}
