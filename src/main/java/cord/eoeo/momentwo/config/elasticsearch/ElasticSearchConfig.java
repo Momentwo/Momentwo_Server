560896b71e366abfa0980bb8624b0f628622beb5
CordHouse <qkqktl5310@gmail.com>
2024-09-14 07:50:29 +0900

    #93 feat : ElasticSearch 설정

---
+++ src/main/java/cord/eoeo/momentwo/config/elasticsearch/ElasticSearchConfig.java
@@ -0,0 +1,27 @@
+package cord.eoeo.momentwo.config.elasticsearch;
+
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import co.elastic.clients.transport.ElasticsearchTransport;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ElasticSearchConfig {
+
+    @Bean
+    public ElasticsearchClient clientConfiguration() {
+        RestClient restClient = RestClient.builder(
+                new HttpHost("localhost", 9200, "http")
+        ).build();
+
+        ElasticsearchTransport transport = new RestClientTransport(
+                restClient, new JacksonJsonpMapper()
+        );
+
+        return new ElasticsearchClient(transport);
+    }
+}
