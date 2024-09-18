b8e540c4ebb0bf47313c90ecaed9ad27787c63c4
CordHouse <qkqktl5310@gmail.com>
2024-09-19 02:09:42 +0900

    #95 feat : 친구 검색 목록 생성

---
+++ src/main/java/cord/eoeo/momentwo/elasticsearch/domain/FriendsDocument.java
@@ -0,0 +1,23 @@
+package cord.eoeo.momentwo.elasticsearch.domain;
+
+import lombok.Data;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+import javax.persistence.Id;
+
+@Data
+@Document(indexName = "friends")
+public class FriendsDocument {
+    @Id
+    private long id;
+
+    @Field(type = FieldType.Keyword)
+    private String toNickname; // 친구 요청을 받은 사람
+
+    public FriendsDocument(long id, String toNickname) {
+        this.id = id;
+        this.toNickname = toNickname;
+    }
+}
