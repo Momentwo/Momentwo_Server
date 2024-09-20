b8bd746edeb0538f17ceff8a096b35a917b5954b
CordHouse <qkqktl5310@gmail.com>
2024-09-20 20:26:41 +0900

    #97 fix : 친구 검색 페이징 적용

--- src/main/java/cord/eoeo/momentwo/elasticsearch/application/service/FriendsSearchService.java
+++ src/main/java/cord/eoeo/momentwo/elasticsearch/application/service/FriendsSearchService.java
@@ -2,13 +2,15 @@ package cord.eoeo.momentwo.elasticsearch.application.service;
 
 import cord.eoeo.momentwo.elasticsearch.adpater.dto.in.UserSearchRequestDto;
 import cord.eoeo.momentwo.elasticsearch.adpater.dto.out.FriendsSearchListResponseDto;
-import cord.eoeo.momentwo.elasticsearch.adpater.out.FriendsElasticSearchManager;
+import cord.eoeo.momentwo.elasticsearch.application.port.out.FriendsElasticSearchManager;
 import cord.eoeo.momentwo.elasticsearch.application.port.in.FriendsSearchUseCase;
 import cord.eoeo.momentwo.user.advice.exception.NotFoundUserException;
 import cord.eoeo.momentwo.user.application.port.out.GetAuthentication;
 import cord.eoeo.momentwo.user.application.port.out.UserRepository;
 import cord.eoeo.momentwo.user.domain.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -25,7 +27,11 @@ public class FriendsSearchService implements FriendsSearchUseCase {
     public FriendsSearchListResponseDto getFriends(UserSearchRequestDto userSearchRequestDto) {
         User user = userRepository.findByNickname(getAuthentication.getAuthentication().getName())
                 .orElseThrow(NotFoundUserException::new);
+
+        Pageable pageable = PageRequest.of(userSearchRequestDto.getPage(), userSearchRequestDto.getSize());
+
         return new FriendsSearchListResponseDto()
-                .toDo(friendsElasticSearchManager.getFriends(userSearchRequestDto.getNickname(), user));
+                .toDo(friendsElasticSearchManager
+                        .getFriendsPaging(userSearchRequestDto.getNickname(), user, pageable));
     }
 }
