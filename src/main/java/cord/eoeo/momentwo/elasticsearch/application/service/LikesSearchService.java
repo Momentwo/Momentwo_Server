329f6d65ea4b8ca7946a3c7a2ddcb367ba336c32
CordHouse <qkqktl5310@gmail.com>
2024-10-01 17:00:00 +0900

    #106 feat : 자신이 누른 좋아요 사진 검색 추가

--- src/main/java/cord/eoeo/momentwo/elasticsearch/application/service/LikesSearchService.java
+++ src/main/java/cord/eoeo/momentwo/elasticsearch/application/service/LikesSearchService.java
@@ -1,10 +1,13 @@
 package cord.eoeo.momentwo.elasticsearch.application.service;
 
 import cord.eoeo.momentwo.elasticsearch.adpater.dto.in.LikesSearchRequestDto;
+import cord.eoeo.momentwo.elasticsearch.adpater.dto.in.LikesStatusSearchRequestDto;
 import cord.eoeo.momentwo.elasticsearch.adpater.dto.out.LikesSearchListResponseDto;
+import cord.eoeo.momentwo.elasticsearch.adpater.dto.out.LikesStatusSearchListResponseDto;
 import cord.eoeo.momentwo.elasticsearch.application.port.in.LikesSearchUseCase;
 import cord.eoeo.momentwo.elasticsearch.application.port.out.LikesElasticSearchManager;
 import cord.eoeo.momentwo.subAlbum.application.aop.annotation.CheckAlbumAccessRules;
+import cord.eoeo.momentwo.user.application.port.out.GetAuthentication;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -14,6 +17,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class LikesSearchService implements LikesSearchUseCase {
     private final LikesElasticSearchManager likesElasticSearchManager;
+    private final GetAuthentication getAuthentication;
 
     @Override
     @CheckAlbumAccessRules
@@ -22,5 +26,20 @@ public class LikesSearchService implements LikesSearchUseCase {
 
         return new LikesSearchListResponseDto()
                 .toDo(likesElasticSearchManager.getPhotoLikesPaging(likesSearchRequestDto.getPhotoId(), pageable));
+    }
+
+    @Override
+    @CheckAlbumAccessRules
+    public LikesStatusSearchListResponseDto getPhotoLikesStatus(LikesStatusSearchRequestDto likesStatusSearchRequestDto) {
+        Pageable pageable = PageRequest
+                .of(likesStatusSearchRequestDto.getPage(), likesStatusSearchRequestDto.getSize());
+
+        return new LikesStatusSearchListResponseDto()
+                .toDo(likesElasticSearchManager
+                        .getPhotoLikesStatus(
+                                likesStatusSearchRequestDto.getSubAlbumId(),
+                                getAuthentication.getAuthentication().getName(),
+                                pageable)
+                );
     }
 }
