054b9dc1f6f5acbb1a312cdbc5253919654fc9fe
CordHouse <qkqktl5310@gmail.com>
2024-10-02 18:07:15 +0900

    #117 feat : 멤버 나가기 요청 DTO

---
+++ src/main/java/cord/eoeo/momentwo/member/adapter/in/dto/MemberOutRequestDto.java
@@ -0,0 +1,12 @@
+package cord.eoeo.momentwo.member.adapter.in.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemberOutRequestDto {
+    private long albumId;
+}
