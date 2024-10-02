72a8b7bc5fc465b879a1816afdfa3cdefe363a7a
CordHouse <qkqktl5310@gmail.com>
2024-10-02 18:08:58 +0900

    #117 fix : 멤버 url 수정

--- src/main/java/cord/eoeo/momentwo/member/application/port/in/AlbumMemberUseCase.java
+++ src/main/java/cord/eoeo/momentwo/member/application/port/in/AlbumMemberUseCase.java
@@ -1,26 +1,23 @@
 package cord.eoeo.momentwo.member.application.port.in;
 
-import cord.eoeo.momentwo.member.adapter.in.dto.AssignAdminRequestDto;
-import cord.eoeo.momentwo.member.adapter.in.dto.EditGradeListRequestDto;
-import cord.eoeo.momentwo.member.adapter.in.dto.InviteMembersRequestDto;
-import cord.eoeo.momentwo.member.adapter.in.dto.KickMembersRequestDto;
+import cord.eoeo.momentwo.member.adapter.in.dto.*;
 import cord.eoeo.momentwo.member.adapter.out.dto.AlbumMemberResponseDto;
 
 public interface AlbumMemberUseCase {
-    void inviteMembers(long albumId, InviteMembersRequestDto inviteMembersRequestDto);
+    void inviteMembers(InviteMembersRequestDto inviteMembersRequestDto);
 
     // 멤버 조회
     AlbumMemberResponseDto getMembers(long albumId);
 
     // 멤버 추방
-    void kickMembers(long albumId, KickMembersRequestDto kickMembersRequestDto);
+    void kickMembers(KickMembersRequestDto kickMembersRequestDto);
 
     // 멤버 권한 변경 (앨범 수정 권한)
-    void editMembersGrade(long albumId, EditGradeListRequestDto editGradeListRequestDto);
+    void editMembersGrade(EditGradeListRequestDto editGradeListRequestDto);
 
     // 관리자 권한 넘기기
-    void assignAdmin(long albumId, AssignAdminRequestDto assignAdminRequestDto);
+    void assignAdmin(AssignAdminRequestDto assignAdminRequestDto);
 
     // 멤버 나가기 (개인)
-    void outAlbum(long albumId);
+    void outAlbum(MemberOutRequestDto memberOutRequestDto);
 }
