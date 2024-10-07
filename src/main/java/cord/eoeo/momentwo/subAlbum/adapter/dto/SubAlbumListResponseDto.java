e52aafc3fd4a019adeaa2bfe59245597aa73de8d
CordHouse <qkqktl5310@gmail.com>
2024-10-08 02:20:07 +0900

    #120 fix : 서브앨범 반환 DTO 수정
    - S3 Url 로 반환될 수 있도록 수정

--- src/main/java/cord/eoeo/momentwo/subAlbum/adapter/dto/SubAlbumListResponseDto.java
+++ src/main/java/cord/eoeo/momentwo/subAlbum/adapter/dto/SubAlbumListResponseDto.java
@@ -4,6 +4,7 @@ import cord.eoeo.momentwo.subAlbum.domain.SubAlbum;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import software.amazon.awssdk.services.s3.S3Client;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -14,9 +15,11 @@ import java.util.stream.Collectors;
 public class SubAlbumListResponseDto {
     private List<SubAlbumResponseDto> subAlbumList;
 
-    public SubAlbumListResponseDto toDo(List<SubAlbum> subAlbums) {
+    public SubAlbumListResponseDto toDo(
+            List<SubAlbum> subAlbums, S3Client s3Client, String bucketName, String imagePath) {
         return new SubAlbumListResponseDto(
-                subAlbums.stream().map(subAlbum -> new SubAlbumResponseDto().toDo(subAlbum))
+                subAlbums.stream()
+                        .map(subAlbum -> new SubAlbumResponseDto().toDo(subAlbum, s3Client, bucketName, imagePath))
                         .collect(Collectors.toList())
         );
     }
--- src/main/java/cord/eoeo/momentwo/subAlbum/adapter/dto/SubAlbumResponseDto.java
+++ src/main/java/cord/eoeo/momentwo/subAlbum/adapter/dto/SubAlbumResponseDto.java
@@ -5,6 +5,7 @@ import cord.eoeo.momentwo.subAlbum.domain.SubAlbum;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import software.amazon.awssdk.services.s3.S3Client;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -17,11 +18,15 @@ public class SubAlbumResponseDto {
     private String subAlbumTitle;
     private List<ImageViewResponseDto> subTitleImageList;
 
-    public SubAlbumResponseDto toDo(SubAlbum subAlbum) {
+    public SubAlbumResponseDto toDo(SubAlbum subAlbum, S3Client s3Client, String buckName, String imagePath) {
         return new SubAlbumResponseDto(
                 subAlbum.getId(),
                 subAlbum.getSubAlbumTitle(),
-                subAlbum.getPhotos().stream().map(photo -> new ImageViewResponseDto().toDo(photo)).limit(4)
+                subAlbum.getPhotos().stream()
+                        .map(photo -> new ImageViewResponseDto()
+                                .toDo(photo, s3Client.utilities().getUrl(b ->
+                                        b.bucket(buckName).key(imagePath + photo.getImageName())).toString()))
+                        .limit(4)
                         .collect(Collectors.toList())
         );
     }
