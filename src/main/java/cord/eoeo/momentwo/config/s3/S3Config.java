c70690590cf4c58f5aff80bb2a6f82d92930f1bf
CordHouse <qkqktl5310@gmail.com>
2024-10-15 17:57:08 +0900

    #169 feat : s3 프리사인드 설정
    - 빈등록

--- src/main/java/cord/eoeo/momentwo/config/s3/S3Config.java
+++ src/main/java/cord/eoeo/momentwo/config/s3/S3Config.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
 public class S3Config {
@@ -23,6 +24,15 @@ public class S3Config {
         return S3Client.builder()
                 .credentialsProvider(StaticCredentialsProvider.create(awsS3))
                 .region(Region.of(region))
+                .build();
+    }
+
+    @Bean
+    public S3Presigner s3Presigner(S3Client s3Client) {
+        AwsBasicCredentials awsS3 = AwsBasicCredentials.create(accessKey, secretKey);
+        return S3Presigner.builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(awsS3))
                 .build();
     }
 }
