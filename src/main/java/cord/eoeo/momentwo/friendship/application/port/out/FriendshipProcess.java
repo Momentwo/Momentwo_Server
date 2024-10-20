d2f2439817c25f669b2f1e89f8127073578010fa
CordHouse <qkqktl5310@gmail.com>
2024-10-21 05:17:56 +0900

    #177 fix : DTO 패키지 상위폴더로 이동

--- src/main/java/cord/eoeo/momentwo/friendship/application/port/out/FriendshipProcess.java
+++ src/main/java/cord/eoeo/momentwo/friendship/application/port/out/FriendshipProcess.java
@@ -1,8 +1,8 @@
 package cord.eoeo.momentwo.friendship.application.port.out;
 
-import cord.eoeo.momentwo.friendship.adapter.in.dto.FriendshipAllListResponseDto;
-import cord.eoeo.momentwo.friendship.adapter.in.dto.FriendshipReceiveListResponseDto;
-import cord.eoeo.momentwo.friendship.adapter.in.dto.FriendshipSendListResponseDto;
+import cord.eoeo.momentwo.friendship.adapter.dto.FriendshipAllListResponseDto;
+import cord.eoeo.momentwo.friendship.adapter.dto.FriendshipReceiveListResponseDto;
+import cord.eoeo.momentwo.friendship.adapter.dto.FriendshipSendListResponseDto;
 import cord.eoeo.momentwo.user.domain.User;
 
 public interface FriendshipProcess {
