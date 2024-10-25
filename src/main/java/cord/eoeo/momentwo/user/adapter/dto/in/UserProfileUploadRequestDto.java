6258d3c1c3bc99f3211505fe9b68a3d6ef45415d
CordHouse <qkqktl5310@gmail.com>
2024-10-25 19:35:45 +0900

    #183 feat : 유저 프로필 요청 DTO

---
+++ src/main/java/cord/eoeo/momentwo/user/adapter/dto/in/UserProfileUploadRequestDto.java
@@ -0,0 +1,12 @@
+package cord.eoeo.momentwo.user.adapter.dto.in;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserProfileUploadRequestDto {
+    private String filename;
+}
