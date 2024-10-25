32009d5fcbe0016ee74a71eafe2f5ebfa01eedaf
CordHouse <qkqktl5310@gmail.com>
2024-10-25 19:11:31 +0900

    #183 feat : 유저 프로필 프리사인드 요청 DTO

---
+++ src/main/java/cord/eoeo/momentwo/image/adapter/dto/UserPresignedRequestDto.java
@@ -0,0 +1,12 @@
+package cord.eoeo.momentwo.image.adapter.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserPresignedRequestDto {
+    private String extension;
+}
