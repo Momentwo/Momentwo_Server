02f630176017a9840ebf119d4ec24de1bf8146a4
CordHouse <qkqktl5310@gmail.com>
2024-10-25 19:12:25 +0900

    #183 feat : 유저 프로필 프리사인드 구현

--- src/main/java/cord/eoeo/momentwo/image/application/port/in/ImageUseCase.java
+++ src/main/java/cord/eoeo/momentwo/image/application/port/in/ImageUseCase.java
@@ -2,8 +2,10 @@ package cord.eoeo.momentwo.image.application.port.in;
 
 import cord.eoeo.momentwo.image.adapter.dto.PresignedRequestDto;
 import cord.eoeo.momentwo.image.adapter.dto.PresignedResponseDto;
+import cord.eoeo.momentwo.image.adapter.dto.UserPresignedRequestDto;
 
 public interface ImageUseCase {
     PresignedResponseDto photoPresignedUrl(PresignedRequestDto presignedRequestDto);
     PresignedResponseDto albumProfilePresignedUrl(PresignedRequestDto presignedRequestDto);
+    PresignedResponseDto userProfilePresignedUrl(UserPresignedRequestDto userPresignedRequestDto);
 }
