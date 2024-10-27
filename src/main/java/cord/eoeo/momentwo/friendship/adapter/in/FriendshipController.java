0a6dde1beb81240e7b9ab77a52b592acf1abbf13
CordHouse <qkqktl5310@gmail.com>
2024-10-28 00:25:32 +0900

    #108 feat : 친구 삭제 컨트롤러 추가

--- src/main/java/cord/eoeo/momentwo/friendship/adapter/in/FriendshipController.java
+++ src/main/java/cord/eoeo/momentwo/friendship/adapter/in/FriendshipController.java
@@ -10,48 +10,55 @@ import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/friendship")
 public class FriendshipController {
     private final FriendshipUseCase friendshipUseCase;
 
     // 요청
-    @PostMapping("/friendship/request")
+    @PostMapping("/request")
     @ResponseStatus(HttpStatus.OK)
     public void requestFriendship(@ModelAttribute @Valid RequestFriendshipDto requestFriendshipDto) {
         friendshipUseCase.requestFriendship(requestFriendshipDto);
     }
 
     // 친구요청에 대한 응답 (수락 & 거절)
-    @PostMapping("/friendship/response")
+    @PostMapping("/response")
     @ResponseStatus(HttpStatus.OK)
     public void responseFriendship(@RequestBody @Valid ResponseFriendshipDto responseFriendshipDto) {
         friendshipUseCase.responseFriendship(responseFriendshipDto);
     }
 
     // 친구 요청 취소
-    @DeleteMapping("/friendship/request/cancel")
+    @DeleteMapping("/request/cancel")
     @ResponseStatus(HttpStatus.OK)
     public void requestFriendshipCancel(@ModelAttribute @Valid RequestFriendshipDto requestFriendshipDto) {
         friendshipUseCase.requestFriendshipCancel(requestFriendshipDto);
     }
 
     // 친구목록 조회
-    @GetMapping("/friendship")
+    @GetMapping("")
     @ResponseStatus(HttpStatus.OK)
     public FriendshipAllListResponseDto getFriendship() {
         return friendshipUseCase.getFriendship();
     }
 
     // 보낸 친구 요청 조회
-    @GetMapping("/friendship/send")
+    @GetMapping("/send")
     @ResponseStatus(HttpStatus.OK)
     public FriendshipSendListResponseDto getFriendshipSend() {
         return friendshipUseCase.getFriendshipSend();
     }
 
     // 받은 친구 요청 조회
-    @GetMapping("/friendship/receive")
+    @GetMapping("/receive")
     @ResponseStatus(HttpStatus.OK)
     public FriendshipReceiveListResponseDto getFriendshipReceive() {
         return friendshipUseCase.getFriendshipReceive();
+    }
+
+    @DeleteMapping("/delete")
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteFriends(@ModelAttribute @Valid RequestFriendshipDto requestFriendshipDto) {
+        friendshipUseCase.deleteFriends(requestFriendshipDto);
     }
 }
