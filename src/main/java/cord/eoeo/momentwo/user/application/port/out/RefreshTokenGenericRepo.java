f82e2324abc89d3c5beb91035421ecd5c6022b53
CordHouse <qkqktl5310@gmail.com>
2024-11-03 00:59:21 +0900

    #208 feat : 리프레시 토큰 제네릭 상속

---
+++ src/main/java/cord/eoeo/momentwo/user/application/port/out/RefreshTokenGenericRepo.java
@@ -0,0 +1,7 @@
+package cord.eoeo.momentwo.user.application.port.out;
+
+import cord.eoeo.momentwo.global.application.port.out.GenericDefaultRepo;
+import cord.eoeo.momentwo.user.domain.RefreshToken;
+
+public interface RefreshTokenGenericRepo extends GenericDefaultRepo<RefreshToken, Long> {
+}
