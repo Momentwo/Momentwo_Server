87a9b85993cd40d6936a8f34e9cf0d8455db1e10
CordHouse <qkqktl5310@gmail.com>
2024-11-02 23:59:12 +0900

    #208 refactor : 회원가입 유스케이스 분리 구현

--- src/main/java/cord/eoeo/momentwo/user/application/port/in/UserRegisterUseCase.java
+++ src/main/java/cord/eoeo/momentwo/user/application/port/in/UserRegisterUseCase.java
@@ -1,17 +1,17 @@
 package cord.eoeo.momentwo.user.application.port.in;
 
-import cord.eoeo.momentwo.user.adapter.dto.in.EmailAvailabilityDto;
-import cord.eoeo.momentwo.user.adapter.dto.in.NicknameAvailabilityDto;
 import cord.eoeo.momentwo.user.adapter.dto.in.UserRegisterRequestDto;
 
-import java.util.concurrent.CompletableFuture;
-
 public interface UserRegisterUseCase {
+    /**
+     * 회원가입
+     * @param userRegisterRequestDto
+     * name : 이름
+     * username : 이메일
+     * password : 비밀번호
+     * nickname : 별명
+     * birthday : 생년월일
+     * phone : 연락처
+     */
     void register(UserRegisterRequestDto userRegisterRequestDto);
-
-    // 지속적인 이메일 중복 유효성 확인
-    CompletableFuture<Void> checkEmailAvailability(EmailAvailabilityDto emailAvailabilityDto);
-
-    // 지속적인 별명 중복 유효성 확인
-    CompletableFuture<Void> checkNicknameAvailability(NicknameAvailabilityDto nicknameAvailabilityDto);
 }
