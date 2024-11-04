ef2121c5a57965b405399cc684b21939bcefd91f
CordHouse <qkqktl5310@gmail.com>
2024-11-04 15:26:54 +0900

    #209 style : 사진 프리사인드 URL 요청 비즈니스 로직

---
+++ src/main/java/cord/eoeo/momentwo/image/application/service/PhotoPresignedUrlService.java
@@ -0,0 +1,36 @@
+package cord.eoeo.momentwo.image.application.service;
+
+import cord.eoeo.momentwo.album.application.port.out.AlbumManager;
+import cord.eoeo.momentwo.album.domain.Album;
+import cord.eoeo.momentwo.config.s3.S3Manager;
+import cord.eoeo.momentwo.image.adapter.dto.PresignedRequestDto;
+import cord.eoeo.momentwo.image.adapter.dto.PresignedResponseDto;
+import cord.eoeo.momentwo.image.application.port.in.PhotoPresignedUrlUseCase;
+import cord.eoeo.momentwo.image.application.port.out.MakeImagePresignedUrlPort;
+import cord.eoeo.momentwo.photo.advice.exception.PhotoCapacityFullException;
+import cord.eoeo.momentwo.photo.application.port.out.PhotoRepository;
+import cord.eoeo.momentwo.subAlbum.application.aop.annotation.CheckAlbumAccessRules;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PhotoPresignedUrlService implements PhotoPresignedUrlUseCase {
+    private final AlbumManager albumManager;
+    private final PhotoRepository photoRepository;
+    private final MakeImagePresignedUrlPort makeImagePresignedUrlPort;
+    private final S3Manager s3Manager;
+
+    @Override
+    @CheckAlbumAccessRules
+    public PresignedResponseDto photoPresignedUrl(PresignedRequestDto presignedRequestDto) {
+        // 앨범 제한 갯수를 넘지 않는 다면 반환
+        Album album = albumManager.getAlbumInfo(presignedRequestDto.getAlbumId());
+        if(photoRepository.getAlbumCount(album) >= 1000) {
+            throw new PhotoCapacityFullException();
+        }
+        return new PresignedResponseDto()
+                .toDo(makeImagePresignedUrlPort
+                        .makeImagePresignedUrl(presignedRequestDto.getExtension(), s3Manager.getImagePath()));
+    }
+}
