aa4ebe1d1e056ac046fd84b2034f65055223c84b
CordHouse <qkqktl5310@gmail.com>
2024-11-04 16:58:22 +0900

    #212 style : 사진 제네릭 레포

---
+++ src/main/java/cord/eoeo/momentwo/photo/application/port/out/PhotoGenericRepo.java
@@ -0,0 +1,7 @@
+package cord.eoeo.momentwo.photo.application.port.out;
+
+import cord.eoeo.momentwo.global.application.port.out.GenericDefaultRepo;
+import cord.eoeo.momentwo.photo.domain.Photo;
+
+public interface PhotoGenericRepo extends GenericDefaultRepo<Photo, Long> {
+}
