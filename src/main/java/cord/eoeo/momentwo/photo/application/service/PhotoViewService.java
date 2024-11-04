e5d847229160ab7b1790b04b1ab6a3fa7184868e
CordHouse <qkqktl5310@gmail.com>
2024-11-04 17:18:07 +0900

    #212 style : 사진 조회 비즈니스 로직

---
+++ src/main/java/cord/eoeo/momentwo/photo/application/service/PhotoViewService.java
@@ -0,0 +1,36 @@
+package cord.eoeo.momentwo.photo.application.service;
+
+import cord.eoeo.momentwo.image.adapter.dto.ImageViewListResponseDto;
+import cord.eoeo.momentwo.photo.advice.exception.NotFoundPhotoException;
+import cord.eoeo.momentwo.photo.application.port.in.PhotoViewUseCase;
+import cord.eoeo.momentwo.photo.application.port.out.PhotoPageRepository;
+import cord.eoeo.momentwo.photo.domain.Photo;
+import cord.eoeo.momentwo.subAlbum.application.aop.annotation.CheckAlbumAccessRules;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class PhotoViewService implements PhotoViewUseCase {
+    private final PhotoPageRepository photoPageRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    @CheckAlbumAccessRules
+    public ImageViewListResponseDto photoView(long albumId, long subAlbumId, int size, long cursor) {
+        Page<Photo> photoList = photoPageRepository
+                .findQPhotoBySubAlbumIdCustomPaging(
+                        subAlbumId,
+                        PageRequest.of((int) (cursor / size), size),
+                        cursor
+                );
+        if(photoList.isEmpty()) {
+            throw new NotFoundPhotoException();
+        }
+
+        return new ImageViewListResponseDto().toDo(photoList);
+    }
+}
