14285fcc37e8e9cdc868def8b7ff708f943c99f6
CordHouse <qkqktl5310@gmail.com>
2024-11-05 22:12:54 +0900

    #213 fix : 재귀 오류 수정

--- src/main/java/cord/eoeo/momentwo/subAlbum/adapter/out/delete/DeleteSubAlbumIdsRepoAdapter.java
+++ src/main/java/cord/eoeo/momentwo/subAlbum/adapter/out/delete/DeleteSubAlbumIdsRepoAdapter.java
@@ -1,6 +1,7 @@
 package cord.eoeo.momentwo.subAlbum.adapter.out.delete;
 
 import cord.eoeo.momentwo.subAlbum.application.port.out.delete.DeleteSubAlbumIdsRepo;
+import cord.eoeo.momentwo.subAlbum.application.port.out.jpa.SubAlbumDeleteJpaRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -9,10 +10,10 @@ import java.util.List;
 @Component
 @RequiredArgsConstructor
 public class DeleteSubAlbumIdsRepoAdapter implements DeleteSubAlbumIdsRepo {
-    private final DeleteSubAlbumIdsRepo deleteSubAlbumIdsRepo;
+    private final SubAlbumDeleteJpaRepo subAlbumDeleteJpaRepo;
 
     @Override
     public void deleteBySubAlbumIds(List<Long> subAlbumIds) {
-        deleteSubAlbumIdsRepo.deleteBySubAlbumIds(subAlbumIds);
+        subAlbumDeleteJpaRepo.deleteBySubAlbumIds(subAlbumIds);
     }
 }
--- src/main/java/cord/eoeo/momentwo/subAlbum/adapter/out/get/GetSubAlbumListByAlbumIdRepoAdapter.java
+++ src/main/java/cord/eoeo/momentwo/subAlbum/adapter/out/get/GetSubAlbumListByAlbumIdRepoAdapter.java
@@ -1,6 +1,7 @@
 package cord.eoeo.momentwo.subAlbum.adapter.out.get;
 
 import cord.eoeo.momentwo.subAlbum.application.port.out.get.GetSubAlbumListByAlbumIdRepo;
+import cord.eoeo.momentwo.subAlbum.application.port.out.jpa.SubAlbumGetJpaRepo;
 import cord.eoeo.momentwo.subAlbum.domain.SubAlbum;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -10,10 +11,10 @@ import java.util.List;
 @Component
 @RequiredArgsConstructor
 public class GetSubAlbumListByAlbumIdRepoAdapter implements GetSubAlbumListByAlbumIdRepo {
-    private final GetSubAlbumListByAlbumIdRepo getSubAlbumListByAlbumIdRepo;
+    private final SubAlbumGetJpaRepo subAlbumGetJpaRepo;
 
     @Override
     public List<SubAlbum> getSubAlbumListByAlbumId(long albumId) {
-        return getSubAlbumListByAlbumIdRepo.getSubAlbumListByAlbumId(albumId);
+        return subAlbumGetJpaRepo.getSubAlbumListByAlbumId(albumId);
     }
 }
