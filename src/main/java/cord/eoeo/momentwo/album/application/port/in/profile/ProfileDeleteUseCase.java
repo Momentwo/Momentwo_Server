b633cc7df648e86c89b51db8c04f02641e1f1db4
CordHouse <qkqktl5310@gmail.com>
2024-11-06 14:07:37 +0900

    #217 style : 앨범 프로필 유스케이스
    #217 style : 앨범 프로필 삭제
    
    #217 style : 앨범 프로필 업데이트

---
+++ src/main/java/cord/eoeo/momentwo/album/application/port/in/profile/ProfileDeleteUseCase.java
@@ -0,0 +1,13 @@
+package cord.eoeo.momentwo.album.application.port.in.profile;
+
+import cord.eoeo.momentwo.album.adapter.dto.AlbumProfileRequestDto;
+
+public interface ProfileDeleteUseCase {
+    /**
+     * 앨범 프로필 삭제
+     * @param albumProfileRequestDto
+     * albumId : 앨범 아이디
+     */
+    // 프로필 삭제
+    void profileDelete(AlbumProfileRequestDto albumProfileRequestDto);
+}
