084b6cf49a541ec111c6122dd1d4f8852c5cfb3a
CordHouse <qkqktl5310@gmail.com>
2024-11-06 22:24:44 +0900

    #218 style : 유저 중 관리자 권한을 갖은 앨범 어댑터

---
+++ src/main/java/cord/eoeo/momentwo/member/adapter/out/info/GetAlbumIdByAdminUserAdapter.java
@@ -0,0 +1,22 @@
+package cord.eoeo.momentwo.member.adapter.out.info;
+
+import cord.eoeo.momentwo.member.application.port.out.info.GetAlbumIdByAdminUserPort;
+import cord.eoeo.momentwo.user.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class GetAlbumIdByAdminUserAdapter implements GetAlbumIdByAdminUserPort {
+    private final GetAlbumIdByAdminUserPort getAlbumIdByAdminUserPort;
+
+    // 유저 중 관리자 권한을 가진 앨범 찾기
+    @Override
+    @Transactional(readOnly = true)
+    public List<Long> getAlbumIdByAdminUser(User user) {
+        return getAlbumIdByAdminUserPort.getAlbumIdByAdminUser(user);
+    }
+}
