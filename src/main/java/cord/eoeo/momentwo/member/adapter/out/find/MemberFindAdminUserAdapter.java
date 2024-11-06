f7786f24acccc3ad307a5afe8a2b4a574b99f5ee
CordHouse <qkqktl5310@gmail.com>
2024-11-06 22:20:41 +0900

    #218 style : 관리자에 해당하는 앨범 찾기 어댑터

---
+++ src/main/java/cord/eoeo/momentwo/member/adapter/out/find/MemberFindAdminUserAdapter.java
@@ -0,0 +1,20 @@
+package cord.eoeo.momentwo.member.adapter.out.find;
+
+import cord.eoeo.momentwo.member.application.port.out.find.MemberFindAdminUserRepo;
+import cord.eoeo.momentwo.member.application.port.out.jpa.AlbumMemberFindJpaRepo;
+import cord.eoeo.momentwo.user.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class MemberFindAdminUserAdapter implements MemberFindAdminUserRepo {
+    private final AlbumMemberFindJpaRepo albumMemberFindJpaRepo;
+
+    @Override
+    public List<Long> findAlbumIdByAdminUser(User user) {
+        return albumMemberFindJpaRepo.findAlbumIdByAdminUser(user);
+    }
+}
