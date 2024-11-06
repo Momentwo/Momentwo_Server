7a1ad519ffccd41b4ad5ea61495ae27520b7e0f3
CordHouse <qkqktl5310@gmail.com>
2024-11-06 16:19:10 +0900

    #217 style : 앨범 서브 타이틀 삭제 포트

---
+++ src/main/java/cord/eoeo/momentwo/album/application/port/out/profile/AlbumSubTitleDeletePort.java
@@ -0,0 +1,7 @@
+package cord.eoeo.momentwo.album.application.port.out.profile;
+
+import cord.eoeo.momentwo.member.domain.Member;
+
+public interface AlbumSubTitleDeletePort {
+    void albumSubTitleDelete(Member member);
+}
