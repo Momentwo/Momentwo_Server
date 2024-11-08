1190290e86dfad782c5fac5897c6ba3ab718958c
CordHouse <qkqktl5310@gmail.com>
2024-11-08 20:15:17 +0900

    #221 refactor : 댓글 조회 컨트롤러

---
+++ src/main/java/cord/eoeo/momentwo/comment/adapter/in/CommentGetController.java
@@ -0,0 +1,27 @@
+package cord.eoeo.momentwo.comment.adapter.in;
+
+import cord.eoeo.momentwo.comment.adapter.dto.out.CommentListResponseDto;
+import cord.eoeo.momentwo.comment.application.port.in.GetCommentUseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+public class CommentGetController {
+    private final GetCommentUseCase getCommentUseCase;
+
+    @GetMapping("/photo/comments/{albumId}/{photoId}")
+    @ResponseStatus(HttpStatus.OK)
+    public CommentListResponseDto getComment(
+            @PathVariable long albumId,
+            @PathVariable long photoId,
+            @RequestParam int size,
+            @RequestParam(required = false, defaultValue = "0") long cursor
+    ) {
+        return getCommentUseCase.getComment(albumId, photoId, size, cursor);
+    }
+}
