32054c8aa15cb55f744a69702f102c61bb980d1e
CordHouse <qkqktl5310@gmail.com>
2024-11-08 20:12:56 +0900

    #221 refactor : 댓글 Find JPA 레포

--- src/main/java/cord/eoeo/momentwo/comment/application/port/out/jpa/CommentFindJpaRepo.java
+++ src/main/java/cord/eoeo/momentwo/comment/application/port/out/jpa/CommentFindJpaRepo.java
@@ -1,13 +1,13 @@
-package cord.eoeo.momentwo.comment.application.port.out;
+package cord.eoeo.momentwo.comment.application.port.out.jpa;
 
+import cord.eoeo.momentwo.comment.application.port.out.CommentGenericJpaRepo;
 import cord.eoeo.momentwo.comment.domain.Comment;
 import cord.eoeo.momentwo.user.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
-public interface CommentJpaRepository extends JpaRepository<Comment, Long> {
+public interface CommentFindJpaRepo extends CommentGenericJpaRepo {
     @Query("SELECT c FROM Comment c WHERE c.id = :id AND c.user = :user")
     Optional<Comment> findByIdAndUser(long id, User user);
 }
