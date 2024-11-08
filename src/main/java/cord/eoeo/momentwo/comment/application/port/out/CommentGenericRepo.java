4098f0bf3ecbc74cacc239753c4b1662fe9f2465
CordHouse <qkqktl5310@gmail.com>
2024-11-08 20:10:16 +0900

    #221 refactor : 댓글 제네릭 레포

---
+++ src/main/java/cord/eoeo/momentwo/comment/application/port/out/CommentGenericRepo.java
@@ -0,0 +1,7 @@
+package cord.eoeo.momentwo.comment.application.port.out;
+
+import cord.eoeo.momentwo.comment.domain.Comment;
+import cord.eoeo.momentwo.global.application.port.out.GenericDefaultRepo;
+
+public interface CommentGenericRepo extends GenericDefaultRepo<Comment, Long> {
+}
