101a0790d9de0be4de8332cdabd767fb6401aa87
CordHouse <qkqktl5310@gmail.com>
2024-11-08 19:34:48 +0900

    #220 refactor : 좋아요 취소 어댑터

---
+++ src/main/java/cord/eoeo/momentwo/like/adapter/out/manager/UnDoLikesAdapter.java
@@ -0,0 +1,46 @@
+package cord.eoeo.momentwo.like.adapter.out.manager;
+
+import cord.eoeo.momentwo.elasticsearch.application.port.out.LikesElasticSearchManager;
+import cord.eoeo.momentwo.like.advice.exception.NotFoundPhotoLikesException;
+import cord.eoeo.momentwo.like.application.port.out.PhotoLikesGenericRepo;
+import cord.eoeo.momentwo.like.application.port.out.find.PhotoLikesFindByPhotoPort;
+import cord.eoeo.momentwo.like.application.port.out.manager.UnDoLikesPort;
+import cord.eoeo.momentwo.like.domain.PhotoLike;
+import cord.eoeo.momentwo.photo.advice.exception.NotFoundPhotoException;
+import cord.eoeo.momentwo.photo.application.port.out.PhotoGenericRepo;
+import cord.eoeo.momentwo.photo.domain.Photo;
+import cord.eoeo.momentwo.user.advice.exception.NotFoundUserException;
+import cord.eoeo.momentwo.user.application.port.out.GetAuthentication;
+import cord.eoeo.momentwo.user.application.port.out.find.UserFindNicknameRepo;
+import cord.eoeo.momentwo.user.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UnDoLikesAdapter implements UnDoLikesPort {
+    private final UserFindNicknameRepo userFindNicknameRepo;
+    private final GetAuthentication getAuthentication;
+    private final LikesElasticSearchManager likesElasticSearchManager;
+    private final PhotoGenericRepo photoGenericRepo;
+    private final PhotoLikesFindByPhotoPort photoLikesFindByPhotoPort;
+    private final PhotoLikesGenericRepo photoLikesGenericRepo;
+
+    @Override
+    public void undoLikes(long photoId) {
+        User user = userFindNicknameRepo.findByNickname(getAuthentication.getAuthentication().getName())
+                .orElseThrow(NotFoundUserException::new);
+
+        // 검색엔진에 좋아요를 누른상태면 삭제 진행
+        if(likesElasticSearchManager.isLikes(user, photoId)) {
+            Photo photo = photoGenericRepo.findById(photoId).orElseThrow(NotFoundPhotoException::new);
+            PhotoLike photoLike = photoLikesFindByPhotoPort
+                    .findByPhoto(photo).orElseThrow(NotFoundPhotoLikesException::new);
+
+            photoLike.setCount(photoLike.getCount() - 1);
+
+            photoLikesGenericRepo.save(photoLike);
+            likesElasticSearchManager.deleteByLikes(photoId, user.getNickname());
+        }
+    }
+}
