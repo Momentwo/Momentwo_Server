54c5cbb5a72cb2797d3678eaa13a79f901b2f3b3
CordHouse <qkqktl5310@gmail.com>
2024-11-09 16:22:13 +0900

    #222 refactor : 설명 제네릭 레포

---
+++ src/main/java/cord/eoeo/momentwo/description/application/port/out/DescriptionGenericRepo.java
@@ -0,0 +1,7 @@
+package cord.eoeo.momentwo.description.application.port.out;
+
+import cord.eoeo.momentwo.description.domain.Description;
+import cord.eoeo.momentwo.global.application.port.out.GenericDefaultRepo;
+
+public interface DescriptionGenericRepo extends GenericDefaultRepo<Description, Long> {
+}
