8871b4af6ea175b5e4b7e1e50e967b9603023ac6
CordHouse <qkqktl5310@gmail.com>
2024-11-09 16:22:18 +0900

    #222 refactor : 설명 제네릭 JPA 레포

---
+++ src/main/java/cord/eoeo/momentwo/description/application/port/out/DescriptionGenericJpaRepo.java
@@ -0,0 +1,7 @@
+package cord.eoeo.momentwo.description.application.port.out;
+
+import cord.eoeo.momentwo.description.domain.Description;
+import cord.eoeo.momentwo.global.application.port.out.GenericDefaultJpaRepo;
+
+public interface DescriptionGenericJpaRepo extends GenericDefaultJpaRepo<Description, Long> {
+}
