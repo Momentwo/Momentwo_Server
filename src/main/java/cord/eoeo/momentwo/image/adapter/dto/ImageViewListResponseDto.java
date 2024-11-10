b1b6daa92be89e169745ae0824836e0bae566ac2
CordHouse <qkqktl5310@gmail.com>
2024-11-10 17:22:48 +0900

    #236 fix : 사진 커스텀 페이징으로 수정

--- src/main/java/cord/eoeo/momentwo/image/adapter/dto/ImageViewListResponseDto.java
+++ src/main/java/cord/eoeo/momentwo/image/adapter/dto/ImageViewListResponseDto.java
@@ -1,10 +1,10 @@
 package cord.eoeo.momentwo.image.adapter.dto;
 
+import cord.eoeo.momentwo.config.page.CursorPage;
 import cord.eoeo.momentwo.photo.domain.Photo;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.domain.Page;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -14,22 +14,12 @@ import java.util.stream.Collectors;
 @NoArgsConstructor
 public class ImageViewListResponseDto {
     private List<ImageViewResponseDto> images;
-    private long page;
-    private long size;
-    private long totalPages;
-    private long totalElements;
-    private boolean hasNext;
-    private boolean hasPrevious;
+    private Object nextCursor;
 
-    public ImageViewListResponseDto toDo(Page<Photo> photos) {
+    public ImageViewListResponseDto toDo(CursorPage<Photo> photos) {
         return new ImageViewListResponseDto(
                 photos.stream().map(photo -> new ImageViewResponseDto().toDo(photo)).collect(Collectors.toList()),
-                photos.getNumber(),
-                photos.getSize(),
-                photos.getTotalPages(),
-                photos.getTotalElements(),
-                photos.hasNext(),
-                photos.hasPrevious()
+                photos.getNextCursor()
         );
     }
 }
