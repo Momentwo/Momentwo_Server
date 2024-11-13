c4260df1fd06b93c1832df626ad5f20c3b606d3e
CordHouse <qkqktl5310@gmail.com>
2024-11-13 17:01:24 +0900

    #243 feat : 좋아요 레디스 레포

---
+++ src/main/java/cord/eoeo/momentwo/like/application/port/out/PhotoLikesRedisGenericRepo.java
@@ -0,0 +1,6 @@
+package cord.eoeo.momentwo.like.application.port.out;
+
+import cord.eoeo.momentwo.global.application.port.out.RedisDefaultRepo;
+
+public interface PhotoLikesRedisGenericRepo extends RedisDefaultRepo<String, Object, Long> {
+}
