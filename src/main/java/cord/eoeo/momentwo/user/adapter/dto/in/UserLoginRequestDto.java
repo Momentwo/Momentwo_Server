c77a2842550bf3dd2d25f76564c913acaa4d1116
CordHouse <qkqktl5310@gmail.com>
2024-11-14 20:45:01 +0900

    #247 fix : 아무런 입력이 없을 때 예외 누락수정

--- src/main/java/cord/eoeo/momentwo/user/adapter/dto/in/UserLoginRequestDto.java
+++ src/main/java/cord/eoeo/momentwo/user/adapter/dto/in/UserLoginRequestDto.java
@@ -3,16 +3,19 @@ package cord.eoeo.momentwo.user.adapter.dto.in;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
 @Data
 @AllArgsConstructor
 public class UserLoginRequestDto {
     // id
+    @NotBlank(message = "이메일을 입력해주세요.")
     @Pattern(regexp = "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$",
             message = "이메일 형식을 지켜주세요.")
     private String username;
     // password
+    @NotBlank(message = "패스워드를 입력해주세요.")
     @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@$!%*#?&])[A-Za-z[0-9]$@$!%*#?&]{8,20}$",
             message = "비밀번호는 8이상 20자리 특수문자, 영문, 숫자 조합 형식을 지켜주세요.")
     private String password;
