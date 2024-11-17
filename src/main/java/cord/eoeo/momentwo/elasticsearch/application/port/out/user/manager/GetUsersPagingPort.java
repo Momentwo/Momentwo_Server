97181a305671237a5e9a320b4a234d405bfea000
CordHouse <qkqktl5310@gmail.com>
2024-11-18 01:11:09 +0900

    #262 refactor : ES 유저 포트 분리

--- src/main/java/cord/eoeo/momentwo/elasticsearch/application/port/out/user/manager/GetUsersPagingPort.java
+++ src/main/java/cord/eoeo/momentwo/elasticsearch/application/port/out/user/manager/GetUsersPagingPort.java
@@ -1,13 +1,10 @@
-package cord.eoeo.momentwo.elasticsearch.application.port.out;
+package cord.eoeo.momentwo.elasticsearch.application.port.out.user.manager;
 
 import cord.eoeo.momentwo.elasticsearch.domain.UserDocument;
 import cord.eoeo.momentwo.user.domain.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-public interface UserElasticSearchManager {
-    void save(User user);
+public interface GetUsersPagingPort {
     Page<UserDocument> getUsersPaging(String keyword, User user, Pageable pageable);
-    void deleteById(long id);
-    void userInfoChange(User user);
 }
