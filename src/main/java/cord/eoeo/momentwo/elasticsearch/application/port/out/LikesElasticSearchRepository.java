8903b0ba289c3b0934d460264781a193c9407251
CordHouse <qkqktl5310@gmail.com>
2024-11-18 01:13:23 +0900

    #262 fix : 삭제

--- src/main/java/cord/eoeo/momentwo/elasticsearch/application/port/out/LikesElasticSearchRepository.java
+++
@@ -1,8 +0,0 @@
-package cord.eoeo.momentwo.elasticsearch.application.port.out;
-
-import cord.eoeo.momentwo.elasticsearch.domain.LikesDocument;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-public interface LikesElasticSearchRepository extends ElasticsearchRepository<LikesDocument, Long> {
-    long countByPhotoId(long photoId);
-}
--- src/main/java/cord/eoeo/momentwo/elasticsearch/application/port/out/UserSearchElasticRepository.java
+++
@@ -1,7 +0,0 @@
-package cord.eoeo.momentwo.elasticsearch.application.port.out;
-
-import cord.eoeo.momentwo.elasticsearch.domain.UserDocument;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-public interface UserSearchElasticRepository extends ElasticsearchRepository<UserDocument, Long> {
-}
