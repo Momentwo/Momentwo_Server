be70f8028d4e4454fa86acfebf02a5b5a0095464
CordHouse <qkqktl5310@gmail.com>
2024-11-20 23:36:26 +0900

    #279 feat : 사진 태그 제네릭 jpa 레포

---
+++ src/main/java/cord/eoeo/momentwo/tag/application/port/out/photo/PhotoTagGenericJpaRepo.java
@@ -0,0 +1,7 @@
+package cord.eoeo.momentwo.tag.application.port.out.photo;
+
+import cord.eoeo.momentwo.global.application.port.out.GenericDefaultJpaRepo;
+import cord.eoeo.momentwo.tag.domain.PhotoTag;
+
+public interface PhotoTagGenericJpaRepo extends GenericDefaultJpaRepo<PhotoTag, Long> {
+}
