00a3a9a23f0c4668c62b4e15198b1e8a8fae813d
CordHouse <qkqktl5310@gmail.com>
2024-11-20 23:32:41 +0900

    #279 feat : 앨범 태그 모두 조회 어댑터

---
+++ src/main/java/cord/eoeo/momentwo/tag/adapter/out/album/manager/AlbumTagGetAllTagAdapter.java
@@ -0,0 +1,19 @@
+package cord.eoeo.momentwo.tag.adapter.out.album.manager;
+
+import cord.eoeo.momentwo.tag.application.port.out.album.jpa.AlbumTagGetJpaRepo;
+import cord.eoeo.momentwo.tag.application.port.out.album.manager.AlbumTagGetAllTagPort;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class AlbumTagGetAllTagAdapter implements AlbumTagGetAllTagPort {
+    private final AlbumTagGetJpaRepo albumTagGetJpaRepo;
+
+    @Override
+    public List<String> allTagByAlbumId(long albumId) {
+        return albumTagGetJpaRepo.allTagByAlbumId(albumId);
+    }
+}
