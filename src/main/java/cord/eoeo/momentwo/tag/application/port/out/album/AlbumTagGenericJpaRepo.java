075b4fa22d3cc3da40fd5926e0efc1190c96e616
CordHouse <qkqktl5310@gmail.com>
2024-11-20 23:26:26 +0900

    #279 feat : 앨범 태그 제네릭 레포

---
+++ src/main/java/cord/eoeo/momentwo/tag/application/port/out/album/AlbumTagGenericJpaRepo.java
@@ -0,0 +1,7 @@
+package cord.eoeo.momentwo.tag.application.port.out.album;
+
+import cord.eoeo.momentwo.global.application.port.out.GenericDefaultJpaRepo;
+import cord.eoeo.momentwo.tag.domain.AlbumTag;
+
+public interface AlbumTagGenericJpaRepo extends GenericDefaultJpaRepo<AlbumTag, Long> {
+}
---
+++ src/main/java/cord/eoeo/momentwo/tag/application/port/out/album/AlbumTagGenericRepo.java
@@ -0,0 +1,7 @@
+package cord.eoeo.momentwo.tag.application.port.out.album;
+
+import cord.eoeo.momentwo.global.application.port.out.GenericDefaultRepo;
+import cord.eoeo.momentwo.tag.domain.AlbumTag;
+
+public interface AlbumTagGenericRepo extends GenericDefaultRepo<AlbumTag, Long> {
+}
