fcb3eb5ab886558940ae4f3b208169230cf6fcbb
CordHouse <qkqktl5310@gmail.com>
2024-11-20 19:12:11 +0900

    #278 fix : 댓글 RequestDto 수정

--- src/main/java/cord/eoeo/momentwo/comment/adapter/dto/in/CommentDeleteRequestDto.java
+++ src/main/java/cord/eoeo/momentwo/comment/adapter/dto/in/CommentDeleteRequestDto.java
@@ -4,10 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class CommentDeleteRequestDto {
-    private long albumId;
-    private long commentId;
+    @NotNull(message = "앨범 아이디 누락")
+    private Long albumId;
+
+    @NotNull(message = "댓글 아이디 누락")
+    private Long commentId;
 }
--- src/main/java/cord/eoeo/momentwo/comment/adapter/dto/in/CommentGetRequestDto.java
+++ src/main/java/cord/eoeo/momentwo/comment/adapter/dto/in/CommentGetRequestDto.java
@@ -4,11 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class CommentGetRequestDto {
-    private long albumId;
-    private long photoId;
-    private long cursorId;
+    @NotNull(message = "앨범 아이디 누락")
+    private Long albumId;
+
+    @NotNull(message = "사진 아이디 누락")
+    private Long photoId;
+
+    @NotNull(message = "커서 아이디 누락")
+    private Long cursorId;
 }
