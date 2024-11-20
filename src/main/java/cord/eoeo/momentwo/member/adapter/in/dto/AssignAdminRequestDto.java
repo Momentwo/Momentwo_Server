646e2a4d38ba7b6483bd9fff9f01cd182d9a0bbd
CordHouse <qkqktl5310@gmail.com>
2024-11-20 19:35:33 +0900

    #278 fix : 멤버 RequestDto 수정

--- src/main/java/cord/eoeo/momentwo/member/adapter/in/dto/AssignAdminRequestDto.java
+++ src/main/java/cord/eoeo/momentwo/member/adapter/in/dto/AssignAdminRequestDto.java
@@ -5,12 +5,15 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class AssignAdminRequestDto {
-    private long albumId;
+    @NotNull(message = "앨범 아이디 누락")
+    private Long albumId;
+
     @NotBlank(message = "별명을 입력해주세요.")
     private String nickname;
 }
--- src/main/java/cord/eoeo/momentwo/member/adapter/in/dto/EditGradeListRequestDto.java
+++ src/main/java/cord/eoeo/momentwo/member/adapter/in/dto/EditGradeListRequestDto.java
@@ -5,13 +5,16 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.util.HashMap;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class EditGradeListRequestDto {
-    private long albumId;
+    @NotNull(message = "앨범 아이디 누락")
+    private Long albumId;
+
     @NotEmpty(message = "권한 변경을 위한 입력이 올바르지 않습니다.")
     private HashMap<String, String> editMemberList;
 }
