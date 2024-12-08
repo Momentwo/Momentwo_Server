a4a1b457e48223b32ea56691dcd0f188abfd4400
CordHouse <qkqktl5310@gmail.com>
2024-12-08 20:46:38 +0900

    #289 fix : 유저 아이디 조회 코드 수정

--- src/main/java/cord/eoeo/momentwo/user/application/service/status/UserSignInService.java
+++ src/main/java/cord/eoeo/momentwo/user/application/service/status/UserSignInService.java
@@ -3,12 +3,11 @@ package cord.eoeo.momentwo.user.application.service.status;
 import cord.eoeo.momentwo.config.security.jwt.TokenProvider;
 import cord.eoeo.momentwo.config.security.jwt.adapter.out.TokenResponseDto;
 import cord.eoeo.momentwo.user.adapter.dto.in.UserLoginRequestDto;
-import cord.eoeo.momentwo.user.advice.exception.NotFoundUserException;
 import cord.eoeo.momentwo.user.advice.exception.PasswordMisMatchException;
 import cord.eoeo.momentwo.user.application.port.in.status.UserSignInUseCase;
 import cord.eoeo.momentwo.user.application.port.out.AuthenticationManager;
 import cord.eoeo.momentwo.user.application.port.out.PasswordEncoder;
-import cord.eoeo.momentwo.user.application.port.out.find.UserFindUsernameRepo;
+import cord.eoeo.momentwo.user.application.port.out.valid.UsernameValidPort;
 import cord.eoeo.momentwo.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -19,7 +18,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 public class UserSignInService implements UserSignInUseCase {
-    private final UserFindUsernameRepo userFindUsernameRepo;
+    private final UsernameValidPort usernameValidPort;
     private final PasswordEncoder passwordEncoder;
     private final AuthenticationManager authenticationManager;
     private final TokenProvider tokenProvider;
@@ -27,9 +26,8 @@ public class UserSignInService implements UserSignInUseCase {
     @Override
     @Transactional
     public TokenResponseDto signIn(UserLoginRequestDto userLoginRequestDto) {
-        // 아이디 확인
-        User user = userFindUsernameRepo.findByUsername(userLoginRequestDto.getUsername())
-                .orElseThrow(NotFoundUserException::new);
+        // 아이디 확인 -> 여기 변경
+        User user = usernameValidPort.usernameValid(userLoginRequestDto.getUsername());
         // 비밀번호 확인
         if(!passwordEncoder.matches(userLoginRequestDto.getPassword(), user.getPassword())) {
             throw new PasswordMisMatchException();
