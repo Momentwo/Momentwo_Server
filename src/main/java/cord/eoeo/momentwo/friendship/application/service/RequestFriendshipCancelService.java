1cc0d8991ce1d711f9157df288da41b18c491dcd
CordHouse <qkqktl5310@gmail.com>
2024-12-13 03:40:28 +0900

    #292 fix : 친구 요청 취소 Uri 변경

--- src/main/java/cord/eoeo/momentwo/friendship/application/service/RequestFriendshipCancelService.java
+++ src/main/java/cord/eoeo/momentwo/friendship/application/service/RequestFriendshipCancelService.java
@@ -1,8 +1,9 @@
 package cord.eoeo.momentwo.friendship.application.service;
 
-import cord.eoeo.momentwo.friendship.adapter.dto.RequestFriendshipDto;
 import cord.eoeo.momentwo.friendship.application.port.in.RequestFriendshipCancelUseCase;
 import cord.eoeo.momentwo.friendship.application.port.out.process.RequestCancelPort;
+import cord.eoeo.momentwo.user.advice.exception.NotFoundUserException;
+import cord.eoeo.momentwo.user.application.port.out.UserGenericRepo;
 import cord.eoeo.momentwo.user.application.port.out.valid.UserNicknameValidPort;
 import cord.eoeo.momentwo.user.domain.User;
 import lombok.RequiredArgsConstructor;
@@ -13,15 +14,16 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class RequestFriendshipCancelService implements RequestFriendshipCancelUseCase {
     private final UserNicknameValidPort userNicknameValidPort;
+    private final UserGenericRepo userGenericRepo;
     private final RequestCancelPort requestCancelPort;
 
     // 친구 요청 취소
     @Override
     @Transactional
-    public void requestFriendshipCancel(RequestFriendshipDto requestFriendshipDto) {
+    public void requestFriendshipCancel(Long userId) {
         User fromUser = userNicknameValidPort.authenticationValid();
         // 사용자 조회
-        User toUser = userNicknameValidPort.userNicknameValid(requestFriendshipDto.getNickname());
+        User toUser = userGenericRepo.findById(userId).orElseThrow(NotFoundUserException::new);
 
         requestCancelPort.requestCancel(fromUser, toUser);
     }
