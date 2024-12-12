26e332a13143942aee57e486a9b92b4399837bbb
CordHouse <qkqktl5310@gmail.com>
2024-12-13 03:23:13 +0900

    #292 fix : 댓글 삭제 Uri 변경

--- src/main/java/cord/eoeo/momentwo/comment/adapter/in/CommentDeleteController.java
+++ src/main/java/cord/eoeo/momentwo/comment/adapter/in/CommentDeleteController.java
@@ -1,24 +1,18 @@
 package cord.eoeo.momentwo.comment.adapter.in;
 
-import cord.eoeo.momentwo.comment.adapter.dto.in.CommentDeleteRequestDto;
 import cord.eoeo.momentwo.comment.application.port.in.DeleteCommentUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.Valid;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 public class CommentDeleteController {
     private final DeleteCommentUseCase deleteCommentUseCase;
 
-    @DeleteMapping("/photo/comments")
+    @DeleteMapping("/albums/{albumId}/sub/photos/comments/{commentId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteComment(@RequestBody @Valid CommentDeleteRequestDto commentDeleteRequestDto) {
-        deleteCommentUseCase.deleteComment(commentDeleteRequestDto);
+    public void deleteComment(@PathVariable Long albumId, @PathVariable Long commentId) {
+        deleteCommentUseCase.deleteComment(albumId, commentId);
     }
 }
--- src/main/java/cord/eoeo/momentwo/comment/application/port/in/DeleteCommentUseCase.java
+++ src/main/java/cord/eoeo/momentwo/comment/application/port/in/DeleteCommentUseCase.java
@@ -1,13 +1,12 @@
 package cord.eoeo.momentwo.comment.application.port.in;
 
-import cord.eoeo.momentwo.comment.adapter.dto.in.CommentDeleteRequestDto;
-
 public interface DeleteCommentUseCase {
     /**
      * 댓글 삭제
-     * @param commentDeleteRequestDto
+     * @param albumId
+     * @param commentId
      * albumId : 앨범 아이디
      * commentId : 댓글 아이디
      */
-    void deleteComment(CommentDeleteRequestDto commentDeleteRequestDto);
+    void deleteComment(Long albumId, Long commentId);
 }
