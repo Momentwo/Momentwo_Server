8946b695a745ed2bda9d2e8fa030e206b5bb2442
CordHouse <qkqktl5310@gmail.com>
2024-12-13 02:05:25 +0900

    #292 fix : 앨범 멤버 추방 Uri 변경
    - 현재 비즈니스 로직이 비동기로 처리되어 있는데 DB 조회 등 이점이 없음.
    - 한 번에 처리할 수 있는 로직을 적용하면 좋을 것 같음

--- src/main/java/cord/eoeo/momentwo/member/adapter/in/KickMembersController.java
+++ src/main/java/cord/eoeo/momentwo/member/adapter/in/KickMembersController.java
@@ -1,15 +1,11 @@
 package cord.eoeo.momentwo.member.adapter.in;
 
-import cord.eoeo.momentwo.member.adapter.in.dto.KickMembersRequestDto;
 import cord.eoeo.momentwo.member.application.port.in.KickMembersUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,9 +13,9 @@ public class KickMembersController {
     private final KickMembersUseCase kickMembersUseCase;
 
     // 멤버 추방
-    @DeleteMapping("/members/kick")
+    @DeleteMapping("/albums/{albumId}/members/kick/{kickUsersId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void kickMembers(@RequestBody @Valid KickMembersRequestDto kickMembersRequestDto) {
-        kickMembersUseCase.kickMembers(kickMembersRequestDto);
+    public void kickMembers(@PathVariable Long albumId, @PathVariable List<Long> kickUsersId) {
+        kickMembersUseCase.kickMembers(albumId, kickUsersId);
     }
 }
