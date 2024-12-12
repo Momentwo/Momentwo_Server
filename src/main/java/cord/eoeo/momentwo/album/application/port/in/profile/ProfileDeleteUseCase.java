654782df777b9a943df006785e52c8168187b0d1
CordHouse <qkqktl5310@gmail.com>
2024-12-13 00:19:43 +0900

    #292 fix : 앨범 프로필 삭제 Uri 변경

--- src/main/java/cord/eoeo/momentwo/album/application/port/in/profile/ProfileDeleteUseCase.java
+++ src/main/java/cord/eoeo/momentwo/album/application/port/in/profile/ProfileDeleteUseCase.java
@@ -1,13 +1,11 @@
 package cord.eoeo.momentwo.album.application.port.in.profile;
 
-import cord.eoeo.momentwo.album.adapter.dto.in.AlbumProfileRequestDto;
-
 public interface ProfileDeleteUseCase {
     /**
      * 앨범 프로필 삭제
-     * @param albumProfileRequestDto
+     * @param albumId
      * albumId : 앨범 아이디
      */
     // 프로필 삭제
-    void profileDelete(AlbumProfileRequestDto albumProfileRequestDto);
+    void profileDelete(Long albumId);
 }
