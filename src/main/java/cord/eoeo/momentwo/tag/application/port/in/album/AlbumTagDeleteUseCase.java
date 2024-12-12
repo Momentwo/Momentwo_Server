e8c66613c87a64cd4806b9084c0d0758c698993e
CordHouse <qkqktl5310@gmail.com>
2024-12-13 03:42:46 +0900

    #292 fix : 앨범 태그 삭제 Uri 변경

--- src/main/java/cord/eoeo/momentwo/tag/application/port/in/album/AlbumTagDeleteUseCase.java
+++ src/main/java/cord/eoeo/momentwo/tag/application/port/in/album/AlbumTagDeleteUseCase.java
@@ -1,7 +1,5 @@
 package cord.eoeo.momentwo.tag.application.port.in.album;
 
-import cord.eoeo.momentwo.tag.adapter.dto.in.album.AlbumTagDeleteRequestDto;
-
 public interface AlbumTagDeleteUseCase {
-    void albumTagDelete(AlbumTagDeleteRequestDto albumTagDeleteRequestDto);
+    void albumTagDelete(Long albumId, Long albumTagId);
 }
