ca7d3d6bc98b38d928dbfcdddfb70a07170aa895
CordHouse <qkqktl5310@gmail.com>
2025-01-11 16:12:57 +0900

    #300 fix : 앨범 태그 DTO 명칭 수정

--- src/main/java/cord/eoeo/momentwo/tag/application/port/out/album/jpa/AlbumTagGetJpaRepo.java
+++ src/main/java/cord/eoeo/momentwo/tag/application/port/out/album/jpa/AlbumTagGetJpaRepo.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface AlbumTagGetJpaRepo extends AlbumTagGenericJpaRepo {
-    @Query("SELECT new cord.eoeo.momentwo.tag.adapter.dto.out.album.AlbumTagResponseDto(at.id, at.tag) FROM AlbumTag at WHERE at.album.id = :albumId")
+    @Query("SELECT new cord.eoeo.momentwo.tag.adapter.dto.out.album.AlbumTagQueryDto(at.id, at.tag) FROM AlbumTag at WHERE at.album.id = :albumId")
     List<AlbumTagQueryDto> allTagByAlbumId(long albumId);
 }
